9e61f048c0d6e9af483460e9a40d44fe991752ab
Julian <81520713+Juliandev02@users.noreply.github.com>
2023-04-09 22:42:19 +0200

    Fixed formattings

--- spkg.py
+++ spkg.py
@@ -20,9 +20,10 @@ class Colors:
     UNDERLINE = '\033[4m'
     RESET = '\033[0m'
 
-with open("./fakeroot/etc/spkg/config.json", "r") as f: 
+
+with open("./fakeroot/etc/spkg/config.json", "r") as f:
     data = json.load(f)
-    
+
 language = data['language']
 
 if language == "de":
@@ -38,12 +39,12 @@ if language == "de":
     Canceled = f"{Fore.RED + Colors.BOLD}[!!!]{Fore.RESET} Prozess wurde abgebrochen!{Colors.RESET}"
     PackageDatabaseNotSynced = f"{Fore.RED + Colors.BOLD}[!]{Fore.RESET} Die Paketdatenbank wurde noch nicht synchronisiert. Führe {Fore.CYAN}spkg sync{Fore.RESET} aus, um die Datenbank zu synchronisieren{Colors.RESET}"
     SearchingDatabaseForPackage = f"{Colors.BOLD}Durchsuche Datenbank nach Paket ...{Colors.RESET}"
-    ContinePackageInstallation1 = f"{Colors.RESET}Das Paket {Fore.CYAN}"
+    ContinePackageInstallation1 = f"{Colors.RESET}Das Paket {Fore.CYAN + Colors.BOLD}"
     ContinePackageInstallation2 = f"{Colors.RESET} wird nun heruntergeladen. \nDafür müssen "
     ContinePackageInstallation3 = f"{Colors.RESET} heruntergeladen werden. Fortfahren? [J/N]{Fore.RESET}{Colors.RESET}"
     Abort = "Abbruch ... "
-    
-    
+
+
 elif language == "en":
     NoArgument = f"{Fore.RED + Colors.BOLD}[E]{Fore.RESET} No Argument passed!{Colors.RESET}"
     PackageNotFound = f"{Fore.RED  + Colors.BOLD}[E]{Fore.RESET} Package not found{Colors.RESET}"
@@ -57,11 +58,11 @@ elif language == "en":
     Canceled = f"{Fore.RED + Colors.BOLD}[!!!]{Fore.RESET} Process canceled!{Colors.RESET}"
     PackageDatabaseNotSynced = f"{Fore.RED + Colors.BOLD}[!]{Fore.RESET} The package database has not been synchronized yet. Run {Fore.CYAN}spkg sync{Fore.RESET} to synchronize the database{Colors.RESET}"
     SearchingDatabaseForPackage = f"{Colors.BOLD}Searching trough the database ...{Colors.RESET}"
-    ContinePackageInstallation1 = f"{Colors.RESET}The package {Fore.CYAN}"
-    ContinePackageInstallation2 = f"{Colors.RESET} will now be downloaded. \nThis requires  "
+    ContinePackageInstallation1 = f"{Colors.RESET}The package {Fore.CYAN + Colors.BOLD}"
+    ContinePackageInstallation2 = f"{Colors.RESET} will now be downloaded. \nThis requires "
     ContinePackageInstallation3 = f"{Colors.RESET} to be downloaded. Continue? [Y/N]{Fore.RESET}{Colors.RESET}"
     Abort = "Aborting ..."
-    
+
 
 def help_en():
     print(f"{Colors.UNDERLINE + Colors.BOLD}Advanced Source Package Managment (spkg) {version}{Colors.RESET}\n")
@@ -78,7 +79,8 @@ def help_en():
     print(f"{Colors.BOLD} -> {Fore.BLUE}update:{Fore.RESET} Checks if an update is available for an installed package{Colors.RESET}")
     print(f"{Colors.BOLD} -> {Fore.BLUE}upgrade:{Fore.RESET} Updates all available package updates{Colors.RESET}")
     print(f"\n{Colors.BOLD}Copyright Juliandev02 2023 (C) - Made with <3")
-    
+
+
 def help_de():
     print(f"{Colors.UNDERLINE + Colors.BOLD}Advanced Source Package Managment (spkg) {version}{Colors.RESET}\n")
     print(f"{Fore.CYAN + Colors.BOLD}Aufruf:{Fore.RESET} spkg {Fore.GREEN}[Befehl]{Fore.RED} <Argument>\n")
@@ -94,13 +96,13 @@ def help_de():
     print(f"{Colors.BOLD} -> {Fore.BLUE}update:{Fore.RESET} Überprüft, ob ein Update für die installierten Pakete verfügbar ist{Colors.RESET}")
     print(f"{Colors.BOLD} -> {Fore.BLUE}upgrade:{Fore.RESET} Aktualisiert alle verfügbaren Paketupdates{Colors.RESET}")
     print(f"\n{Colors.BOLD}Copyright Juliandev02 2023 (C) - Made with <3")
-    
+
 
 db = sql.connect("./fakeroot/etc/spkg/package.db")
 c = db.cursor()
 
 
-# * --- Package Info Function --- * 
+# * --- Package Info Function --- *
 if len(sys.argv) > 1 and sys.argv[1] == "info":
     if len(sys.argv) > 2:
         pkg_name = sys.argv[2]
@@ -109,16 +111,18 @@ if len(sys.argv) > 1 and sys.argv[1] == "info":
         print(NoArgument)
         exit()
     try:
-        c.execute("SELECT name FROM packages where name = ?" , (pkg_name,))
-    
+        c.execute("SELECT name FROM packages where name = ?", (pkg_name,))
+
     except OperationalError:
         print(PackageDatabaseNotSynced)
         exit()
 
     if c.fetchall():
-        c.execute("SELECT name, version, branch, arch, fetch_url, setup_script FROM packages where name = ?", (pkg_name,))
+        c.execute(
+            "SELECT name, version, branch, arch, fetch_url, setup_script FROM packages where name = ?", (pkg_name,))
         for row in c:
-            print(f"{Colors.BOLD + Colors.UNDERLINE}{PackageInformationTitle} {row[0]} ({row[1]})\n{Colors.RESET}")
+            print(
+                f"{Colors.BOLD + Colors.UNDERLINE}{PackageInformationTitle} {row[0]} ({row[1]})\n{Colors.RESET}")
             print("Name:", row[0])
             print("Version:", row[1])
             print("Branch:", row[2])
@@ -132,7 +136,7 @@ if len(sys.argv) > 1 and sys.argv[1] == "info":
     db.close()
 
 
-# * --- Download Function --- * 
+# * --- Download Function --- *
 elif len(sys.argv) > 1 and sys.argv[1] == "download":
     if len(sys.argv) > 2:
         pkg_name = sys.argv[2]
@@ -140,10 +144,11 @@ elif len(sys.argv) > 1 and sys.argv[1] == "download":
     else:
         print(NoArgument)
         exit()
-        
-    try:                  
-        c.execute("SELECT name, fetch_url, file_name FROM packages where name = ?" , (pkg_name,))
-        
+
+    try:
+        c.execute(
+            "SELECT name, fetch_url, file_name FROM packages where name = ?", (pkg_name,))
+
     except OperationalError:
         print(PackageDatabaseNotSynced)
         exit()
@@ -162,35 +167,35 @@ elif len(sys.argv) > 1 and sys.argv[1] == "download":
         )
 
         f = urllib.request.urlopen(req)
-        
+
         download_time_start = time.time()
 
-        spinner = Halo(text=f"{StrGet}: {url}", spinner={'interval': 150, 'frames': ['[-]', '[\\]', '[|]', '[/]']}, text_color="white", color="green")
+        spinner = Halo(text=f"{StrGet}: {url}", spinner={'interval': 150, 'frames': [
+                       '[-]', '[\\]', '[|]', '[/]']}, text_color="white", color="green")
         spinner.start()
-        
+
         with open(filename, 'wb') as file:
             file.write(f.read())
 
         download_time_end = time.time()
         print(f"\n{FinishedDownloading} {Fore.LIGHTCYAN_EX + Colors.BOLD}{filename}{Colors.RESET} in {round(download_time_end - download_time_start, 2)} s{Colors.RESET}")
-        
+
     except HTTPError as e:
         print(UnknownError)
         print(e)
 
-    except NameError as e: 
+    except NameError as e:
         print(PackageNotFound)
 
 
-# * --- Sync Function --- * 
-elif len(sys.argv) > 1 and sys.argv[1] == "sync":        
-    with open("./fakeroot/etc/spkg/repositories.json", "r") as f: 
+# * --- Sync Function --- *
+elif len(sys.argv) > 1 and sys.argv[1] == "sync":
+    with open("./fakeroot/etc/spkg/repositories.json", "r") as f:
         data = json.load(f)
-    
-    
+
     repo = f"{data['main']}/package.db"
     filename = "./fakeroot/etc/spkg/package.db"
-    
+
     try:
         req = urllib.request.Request(
             repo,
@@ -201,35 +206,37 @@ elif len(sys.argv) > 1 and sys.argv[1] == "sync":
         )
 
         f = urllib.request.urlopen(req)
-        
+
         download_time_start = time.time()
 
-        spinner = Halo(text=f"{SyncingPackageDatabase} {data['main']} ...", spinner={'interval': 150, 'frames': ['[-]', '[\\]', '[|]', '[/]']}, text_color="white", color="green")
+        spinner = Halo(text=f"{SyncingPackageDatabase} {data['main']} ...", spinner={
+                       'interval': 150, 'frames': ['[-]', '[\\]', '[|]', '[/]']}, text_color="white", color="green")
         spinner.start()
-        
+
         with open(filename, 'wb') as file:
             file.write(f.read())
 
         download_time_end = time.time()
         print(f"\n{SuccessSyncingPackageDatabase}{Colors.RESET}")
-        
+
     except HTTPError as e:
         print(UnknownError)
         print(e)
-        
 
-# * --- List Function --- * 
+
+# * --- List Function --- *
 elif len(sys.argv) > 1 and sys.argv[1] == "list":
     try:
         c.execute("SELECT * FROM packages")
         for row in c:
-                print(f"{Fore.GREEN + Colors.BOLD}{row[0]} {Fore.RESET + Colors.RESET}({row[1]}) @ {Fore.CYAN}{row[2]}{Fore.RESET}")
-                
+            print(
+                f"{Fore.GREEN + Colors.BOLD}{row[0]} {Fore.RESET + Colors.RESET}({row[1]}) @ {Fore.CYAN}{row[2]}{Fore.RESET}")
+
     except OperationalError:
         print(PackageDatabaseNotSynced)
 
 
-# * --- Install Function --- * 
+# * --- Install Function --- *
 elif len(sys.argv) > 1 and sys.argv[1] == "install":
     if len(sys.argv) > 2:
         pkg_name = sys.argv[2]
@@ -237,46 +244,48 @@ elif len(sys.argv) > 1 and sys.argv[1] == "install":
     else:
         print(NoArgument)
         exit()
-    
-    spinner_db_search = Halo(text=f"{SearchingDatabaseForPackage}", spinner={'interval': 150, 'frames': ['[-]', '[\\]', '[|]', '[/]']}, text_color="white", color="green")
+
+    spinner_db_search = Halo(text=f"{SearchingDatabaseForPackage}", spinner={
+                             'interval': 150, 'frames': ['[-]', '[\\]', '[|]', '[/]']}, text_color="white", color="green")
     spinner_db_search.start()
-    
-    
+
     try:
-        c.execute("SELECT name, fetch_url, file_name FROM packages where name = ?" , (pkg_name,))
-        
+        c.execute(
+            "SELECT name, fetch_url, file_name FROM packages where name = ?", (pkg_name,))
+
     except OperationalError:
         print(PackageDatabaseNotSynced)
         exit()
-        
+
     for row in c:
         url = row[1]
         filename = row[2]
-        
-        
+
         response = requests.head(url)
         file_size_bytes = int(response.headers.get('Content-Length', 0))
         file_size_mb = file_size_bytes / (1024 * 1024)
-        
+
         spinner_db_search.stop()
-        print(f"{Fore.GREEN}[/] {Fore.RESET}{SearchingDatabaseForPackage}")
+        print(
+            f"{Fore.GREEN + Colors.BOLD}[/] {Fore.RESET + Colors.RESET}{SearchingDatabaseForPackage}")
         try:
-            continue_pkg_installation = input(f"{ContinePackageInstallation1}{filename}{Colors.RESET}{ContinePackageInstallation2}{round(file_size_mb, 2)} MB{ContinePackageInstallation3} ")
-            
+            continue_pkg_installation = input(
+                f"{ContinePackageInstallation1}{filename}{Colors.RESET}{ContinePackageInstallation2}{round(file_size_mb, 2)} MB{ContinePackageInstallation3} ")
+
         except KeyboardInterrupt as e:
-            print(f"\n{Canceled}") 
+            print(f"\n{Canceled}")
             exit()
-            
-        if continue_pkg_installation.lower() == "j": 
+
+        if continue_pkg_installation.lower() == "j":
             continue
-        
-        elif continue_pkg_installation.lower() == "y": 
+
+        elif continue_pkg_installation.lower() == "y":
             continue
-        
+
         else:
             print(Abort)
             exit()
-        
+
     try:
         req = urllib.request.Request(
             url,
@@ -287,12 +296,13 @@ elif len(sys.argv) > 1 and sys.argv[1] == "install":
         )
 
         f = urllib.request.urlopen(req)
-        
+
         download_time_start = time.time()
 
-        spinner = Halo(text=f"{StrGet}: {url}", spinner={'interval': 150, 'frames': ['[-]', '[\\]', '[|]', '[/]']}, text_color="white", color="green")
+        spinner = Halo(text=f"{StrGet}: {url}", spinner={'interval': 150, 'frames': [
+                       '[-]', '[\\]', '[|]', '[/]']}, text_color="white", color="green")
         spinner.start()
-        
+
         with open(f"/tmp/{filename}", 'wb') as file:
             file.write(f.read())
 
@@ -302,27 +312,26 @@ elif len(sys.argv) > 1 and sys.argv[1] == "install":
     except HTTPError as e:
         print(UnknownError)
         print(e)
-        
 
-    except NameError as e: 
-        print(f"\n{PackageNotFound}") 
+    except NameError as e:
+        print(f"\n{PackageNotFound}")
 
     except KeyboardInterrupt as e:
-        print(f"\n{Canceled}") 
+        print(f"\n{Canceled}")
 
 
-# * --- Help Function --- * 
+# * --- Help Function --- *
 elif len(sys.argv) > 1 and sys.argv[1] == "help":
     if language == "en":
         help_en()
     elif language == "de":
         help_de()
-    
+
 else:
     if language == "en":
         help_en()
     elif language == "de":
         help_de()
-        
-    
+
+
 db.close()
