d5121aa9436d87f9418761b5c0744dff4e12de8a
Julian <81520713+Juliandev02@users.noreply.github.com>
2023-04-11 01:24:08 +0200

    Full update v1.2.0

--- spkg.py
+++ spkg.py
@@ -6,6 +6,7 @@ import time
 import json
 import sqlite3 as sql
 import urllib.request
+import platform
 
 from sqlite3 import *
 from urllib.error import HTTPError
@@ -15,8 +16,8 @@ from sys import exit
 import requests
 import subprocess
 
-version = "1.1.4"
-
+version = "1.2.0"
+world_database = "./fakeroot/etc/spkg/world.db"
 
 class Colors:
     BOLD = '\033[1m'
@@ -52,6 +53,10 @@ if language == "de":
     ExecutingSetup = f"Setup Script wird ausgeführt... Bitte warten"
     MissingPermissons = f"{Colors.BOLD}Fehlende Berechtigung{Colors.RESET}"
     MissingPermissonsPackageDatabaseUpdate = f"{Fore.RED + Colors.BOLD}Die Paketdatenbank konnte nicht aktualisiert werden. (Wird spkg als Root ausgeführt?){Colors.RESET}"
+    SearchingForUpdates = f"Suche nach verfügbaren Updates ..."
+    WorldDatabaseNotBuilded = f"{Fore.RED + Colors.BOLD}[!]{Fore.RESET} Die lokale World Datenbank wurde noch nicht aufgebaut. Ist deine spkg Installation korrupt?{Colors.RESET}"
+    PackageAlreadyInstalled = f"{Fore.YELLOW + Colors.BOLD}[!]{Fore.RESET} Paket wurde bereits installiert. Es gibt nichts zu tun.{Colors.RESET}"
+    PackageNotInstalled = PackageAlreadyInstalled = f"{Fore.YELLOW + Colors.BOLD}[!]{Fore.RESET} Paket ist nicht installiert, es gibt nichts zu aktualisieren.{Colors.RESET}"
 
 
 elif language == "en":
@@ -74,10 +79,14 @@ elif language == "en":
     ExecutingSetup = f"Executing Setup Script... Please wait"
     MissingPermissons = f"{Colors.BOLD}Missing Permissons{Colors.RESET}"
     MissingPermissonsPackageDatabaseUpdate = f"{Fore.RED + Colors.BOLD}The package database could not be updated. (Is spkg running as root?){Colors.RESET}"
+    SearchingForUpdates = f"Suche nach verfügbaren Updates ..."
+    WorldDatabaseNotBuilded = f"{Fore.RED + Colors.BOLD}[!]{Fore.RESET} The local world database has not been built yet. Is your spkg installation corrupt?{Colors.RESET}"
+    PackageAlreadyInstalled = f"{Fore.YELLOW + Colors.BOLD}[!]{Fore.RESET} Package has already been installed. There is nothing to do.{Colors.RESET}"
+    PackageNotInstalled = PackageAlreadyInstalled = f"{Fore.YELLOW + Colors.BOLD}[!]{Fore.RESET} Package is not installed, there is nothing to upgrade.{Colors.RESET}"
 
 
 def help_en():
-    print(f"{Colors.UNDERLINE + Colors.BOLD}Advanced Source Package Managment (spkg) {version}{Colors.RESET}\n")
+    print(f"{Colors.UNDERLINE + Colors.BOLD}Advanced Source Package Managment (spkg) {version} {platform.machine()}{Colors.RESET}\n")
     print(
         f"{Fore.CYAN + Colors.BOLD}Usage:{Fore.RESET} spkg {Fore.GREEN}[command]{Fore.RED} <argument>\n")
     print(f"spkg is a package manager that downloads the source code from the \nofficial sources, and then compiles it specifically for your device.")
@@ -88,14 +97,15 @@ def help_en():
     print(f"{Colors.BOLD} -> {Fore.BLUE}info:{Fore.RESET} Gives you information about a specific package{Colors.RESET}")
     print(f"{Colors.BOLD} -> {Fore.BLUE}download:{Fore.RESET} Downloads a specific package{Colors.RESET}")
     print(f"{Colors.BOLD} -> {Fore.BLUE}install:{Fore.RESET} Installs the specified package{Colors.RESET}")
+    print(f"{Colors.BOLD} -> {Fore.BLUE}install:{Fore.RESET} Reinstalls the specified package{Colors.RESET}")
     print(f"{Colors.BOLD} -> {Fore.BLUE}sync:{Fore.RESET} Syncronizes the package database{Colors.RESET}")
     print(f"{Colors.BOLD} -> {Fore.BLUE}update:{Fore.RESET} Checks if an update is available for an installed package{Colors.RESET} (not available yet)")
-    print(f"{Colors.BOLD} -> {Fore.BLUE}upgrade:{Fore.RESET} Updates all available package updates{Colors.RESET} (not available yet)")
+    print(f"{Colors.BOLD} -> {Fore.BLUE}upgrade:{Fore.RESET} Updates all available package updates{Colors.RESET}")
     print(f"\n{Colors.BOLD}Copyright Juliandev02 2023 (C) - Made with <3")
 
 
 def help_de():
-    print(f"{Colors.UNDERLINE + Colors.BOLD}Advanced Source Package Managment (spkg) {version}{Colors.RESET}\n")
+    print(f"{Colors.UNDERLINE + Colors.BOLD}Advanced Source Package Managment (spkg) {version} {platform.machine()}{Colors.RESET}\n")
     print(
         f"{Fore.CYAN + Colors.BOLD}Aufruf:{Fore.RESET} spkg {Fore.GREEN}[Befehl]{Fore.RED} <Argument>\n")
     print(f"spkg ist ein Paketmanager, der den Quellcode von den \noffiziellen Quellen herunterlädt, und diesen dann spezifisch für dein Gerät kompiliert.")
@@ -106,14 +116,30 @@ def help_de():
     print(f"{Colors.BOLD} -> {Fore.BLUE}info:{Fore.RESET} Gibt dir Informationen über ein bestimmtes Paket aus{Colors.RESET}")
     print(f"{Colors.BOLD} -> {Fore.BLUE}download:{Fore.RESET} Lädt ein bestimmtes Paket herunter{Colors.RESET}")
     print(f"{Colors.BOLD} -> {Fore.BLUE}install:{Fore.RESET} Installiert das angegebene Paket{Colors.RESET}")
+    print(f"{Colors.BOLD} -> {Fore.BLUE}reinstall:{Fore.RESET} Installiert das angegebene Paket neu{Colors.RESET}")
     print(f"{Colors.BOLD} -> {Fore.BLUE}sync:{Fore.RESET} Syncronisiert die Paketdatenbank{Colors.RESET}")
     print(f"{Colors.BOLD} -> {Fore.BLUE}update:{Fore.RESET} Überprüft, ob ein Update für die installierten Pakete verfügbar ist{Colors.RESET} (Noch nicht verfügbar)")
-    print(f"{Colors.BOLD} -> {Fore.BLUE}upgrade:{Fore.RESET} Aktualisiert alle verfügbaren Paketupdates{Colors.RESET} (Noch nicht verfügbar)")
+    print(f"{Colors.BOLD} -> {Fore.BLUE}upgrade:{Fore.RESET} Aktualisiert alle verfügbaren Paketupdates{Colors.RESET}")
     print(f"\n{Colors.BOLD}Copyright Juliandev02 2023 (C) - Made with <3")
 
 
-db = sql.connect("/etc/spkg/package.db")
-c = db.cursor()
+try:
+    db = sql.connect("/etc/spkg/package.db")
+    c = db.cursor()
+except OperationalError:
+    print(PackageDatabaseNotSynced)
+    exit()
+        
+if not os.path.exists(world_database):
+    print(WorldDatabaseNotBuilded)
+    exit()
+    
+try:
+    world_db = sql.connect(world_database)
+    world_c = world_db.cursor()
+except OperationalError:
+    print(WorldDatabaseNotBuilded)
+    exit()
 
 
 # * --- Package Info Function --- *
@@ -266,6 +292,19 @@ elif len(sys.argv) > 1 and sys.argv[1] == "install":
     else:
         print(NoArgument)
         exit()
+    
+    try:
+        world_c.execute("SELECT name from world where name = ?", (pkg_name,))
+    
+    except OperationalError as e:
+        print(WorldDatabaseNotBuilded)
+        exit()
+    
+    if world_c.fetchall():
+        print(PackageAlreadyInstalled)
+        exit()
+    else:
+        ""
 
     spinner_db_search = Halo(text=f"{SearchingDatabaseForPackage}", spinner={
                              'interval': 150, 'frames': ['[-]', '[\\]', '[|]', '[/]']}, text_color="white", color="green")
@@ -354,9 +393,25 @@ elif len(sys.argv) > 1 and sys.argv[1] == "install":
             f"{Fore.GREEN + Colors.BOLD}[/] {Fore.RESET + Colors.RESET}{ExecutingSetup}")
 
         spinner.stop()
+        
         subprocess.run(['sudo', 'chmod', '+x', f'/tmp/{row[0]}.setup'])
         subprocess.run(['sudo', 'bash', f'/tmp/{row[0]}.setup'])
+        
+        try:
+            c.execute("SELECT name, version FROM packages where name = ?", (pkg_name,))
+            for row in c:
+                name = row[0]
+                version = row[1]
 
+        except OperationalError:
+            print(PackageDatabaseNotSynced)
+            exit()
+    
+        world_c.execute("INSERT INTO world (name, version) VALUES (?, ?)", (name, version))
+        world_db.commit()
+        world_db.close()
+    
+    
     except HTTPError as e:
         print(UnknownError)
         print(e)
@@ -366,6 +421,317 @@ elif len(sys.argv) > 1 and sys.argv[1] == "install":
 
     except KeyboardInterrupt as e:
         print(f"\n{Canceled}")
+
+
+# * --- Upgrade Function --- *
+elif len(sys.argv) > 1 and sys.argv[1] == "upgrade":
+    if len(sys.argv) > 2:
+        pkg_name = sys.argv[2]
+
+    else:
+        print(NoArgument)
+        exit()
+    
+    try:
+        world_c.execute("SELECT name from world where name = ?", (pkg_name,))
+    
+    except OperationalError as e:
+        print(WorldDatabaseNotBuilded)
+        exit()
+    
+    if not world_c.fetchall():
+        print(PackageNotInstalled)
+        exit()
+    else:
+        ""
+
+    spinner_db_search = Halo(text=f"{SearchingDatabaseForPackage}", spinner={
+                             'interval': 150, 'frames': ['[-]', '[\\]', '[|]', '[/]']}, text_color="white", color="green")
+    spinner_db_search.start()
+
+    try:
+        c.execute(
+            "SELECT name, fetch_url, file_name, setup_script FROM packages where name = ?", (pkg_name,))
+
+    except OperationalError:
+        print(PackageDatabaseNotSynced)
+        exit()
+
+    for row in c:
+        url = row[1]
+        filename = row[2]
+        setup_script = row[3]
+
+        response = requests.head(url)
+        file_size_bytes = int(response.headers.get('Content-Length', 0))
+        file_size_mb = file_size_bytes / (1024 * 1024)
+
+        spinner_db_search.stop()
+        print(
+            f"{Fore.GREEN + Colors.BOLD}[/] {Fore.RESET + Colors.RESET}{SearchingDatabaseForPackage}")
+        try:
+            continue_pkg_installation = input(
+                f"{ContinePackageInstallation1}{filename}{Colors.RESET}{ContinePackageInstallation2}{round(file_size_mb, 2)} MB{ContinePackageInstallation3} ")
+
+        except KeyboardInterrupt as e:
+            print(f"\n{Canceled}")
+            exit()
+
+        if continue_pkg_installation.lower() == "j":
+            continue
+
+        elif continue_pkg_installation.lower() == "y":
+            continue
+
+        else:
+            print(Abort)
+            exit()
+
+    try:
+        req = urllib.request.Request(
+            url,
+            data=None,
+            headers={
+                'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'
+            }
+        )
+
+        f = urllib.request.urlopen(req)
+
+        download_time_start = time.time()
+
+        spinner = Halo(text=f"{StrGet}: {url}", spinner={'interval': 150, 'frames': [
+                       '[-]', '[\\]', '[|]', '[/]']}, text_color="white", color="green")
+        spinner.start()
+
+        with open(f"/tmp/{filename}", 'wb') as file:
+            file.write(f.read())
+
+        download_time_end = time.time()
+        print(f"\n{FinishedDownloading} {Fore.LIGHTCYAN_EX + Colors.BOLD}{filename}{Colors.RESET} in {round(download_time_end - download_time_start, 2)} s{Colors.RESET}")
+
+        spinner_setup = Halo(text=f"{ExecutingSetup}: {url}", spinner={'interval': 150, 'frames': [
+            '[-]', '[\\]', '[|]', '[/]']}, text_color="white", color="green")
+        spinner_setup.start()
+
+        setup_req = urllib.request.Request(
+            setup_script,
+            data=None,
+            headers={
+                'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'
+            }
+        )
+
+        f_setup = urllib.request.urlopen(setup_req)
+
+        with open(f"/tmp/{row[0]}.setup", 'wb') as file_setup:
+            file_setup.write(f_setup.read())
+
+        spinner_setup.stop()
+        print(
+            f"{Fore.GREEN + Colors.BOLD}[/] {Fore.RESET + Colors.RESET}{ExecutingSetup}")
+
+        spinner.stop()
+        
+        subprocess.run(['sudo', 'chmod', '+x', f'/tmp/{row[0]}.setup'])
+        subprocess.run(['sudo', 'bash', f'/tmp/{row[0]}.setup', "--upgrade"])
+        
+        try:
+            c.execute("SELECT name, version FROM packages where name = ?", (pkg_name,))
+            for row in c:
+                name = row[0]
+                version = row[1]
+
+        except OperationalError:
+            print(PackageDatabaseNotSynced)
+            exit()
+            
+                
+    except HTTPError as e:
+        print(UnknownError)
+        print(e)
+
+    except NameError as e:
+        print(f"\n{PackageNotFound}")
+
+    except KeyboardInterrupt as e:
+        print(f"\n{Canceled}")
+
+
+# * --- Reinstall Function --- *
+elif len(sys.argv) > 1 and sys.argv[1] == "reinstall":
+    if len(sys.argv) > 2:
+        pkg_name = sys.argv[2]
+
+    else:
+        print(NoArgument)
+        exit()
+    
+    try:
+        world_c.execute("SELECT name from world where name = ?", (pkg_name,))
+    
+    except OperationalError as e:
+        print(WorldDatabaseNotBuilded)
+        exit()
+    
+
+    spinner_db_search = Halo(text=f"{SearchingDatabaseForPackage}", spinner={
+                             'interval': 150, 'frames': ['[-]', '[\\]', '[|]', '[/]']}, text_color="white", color="green")
+    spinner_db_search.start()
+
+    try:
+        c.execute(
+            "SELECT name, fetch_url, file_name, setup_script FROM packages where name = ?", (pkg_name,))
+
+    except OperationalError:
+        print(PackageDatabaseNotSynced)
+        exit()
+
+    for row in c:
+        url = row[1]
+        filename = row[2]
+        setup_script = row[3]
+
+        response = requests.head(url)
+        file_size_bytes = int(response.headers.get('Content-Length', 0))
+        file_size_mb = file_size_bytes / (1024 * 1024)
+
+        spinner_db_search.stop()
+        print(
+            f"{Fore.GREEN + Colors.BOLD}[/] {Fore.RESET + Colors.RESET}{SearchingDatabaseForPackage}")
+        try:
+            continue_pkg_installation = input(
+                f"{ContinePackageInstallation1}{filename}{Colors.RESET}{ContinePackageInstallation2}{round(file_size_mb, 2)} MB{ContinePackageInstallation3} ")
+
+        except KeyboardInterrupt as e:
+            print(f"\n{Canceled}")
+            exit()
+
+        if continue_pkg_installation.lower() == "j":
+            continue
+
+        elif continue_pkg_installation.lower() == "y":
+            continue
+
+        else:
+            print(Abort)
+            exit()
+
+    try:
+        req = urllib.request.Request(
+            url,
+            data=None,
+            headers={
+                'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'
+            }
+        )
+
+        f = urllib.request.urlopen(req)
+
+        download_time_start = time.time()
+
+        spinner = Halo(text=f"{StrGet}: {url}", spinner={'interval': 150, 'frames': [
+                       '[-]', '[\\]', '[|]', '[/]']}, text_color="white", color="green")
+        spinner.start()
+
+        with open(f"/tmp/{filename}", 'wb') as file:
+            file.write(f.read())
+
+        download_time_end = time.time()
+        print(f"\n{FinishedDownloading} {Fore.LIGHTCYAN_EX + Colors.BOLD}{filename}{Colors.RESET} in {round(download_time_end - download_time_start, 2)} s{Colors.RESET}")
+
+        spinner_setup = Halo(text=f"{ExecutingSetup}: {url}", spinner={'interval': 150, 'frames': [
+            '[-]', '[\\]', '[|]', '[/]']}, text_color="white", color="green")
+        spinner_setup.start()
+
+        setup_req = urllib.request.Request(
+            setup_script,
+            data=None,
+            headers={
+                'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'
+            }
+        )
+
+        f_setup = urllib.request.urlopen(setup_req)
+
+        with open(f"/tmp/{row[0]}.setup", 'wb') as file_setup:
+            file_setup.write(f_setup.read())
+
+        spinner_setup.stop()
+        print(
+            f"{Fore.GREEN + Colors.BOLD}[/] {Fore.RESET + Colors.RESET}{ExecutingSetup}")
+
+        spinner.stop()
+        
+        subprocess.run(['sudo', 'chmod', '+x', f'/tmp/{row[0]}.setup'])
+        subprocess.run(['sudo', 'bash', f'/tmp/{row[0]}.setup'])
+        
+        try:
+            c.execute("SELECT name, version FROM packages where name = ?", (pkg_name,))
+            for row in c:
+                name = row[0]
+                version = row[1]
+
+        except OperationalError:
+            print(PackageDatabaseNotSynced)
+            exit()
+            
+                
+    except HTTPError as e:
+        print(UnknownError)
+        print(e)
+
+    except NameError as e:
+        print(f"\n{PackageNotFound}")
+
+    except KeyboardInterrupt as e:
+        print(f"\n{Canceled}")
+
+
+# * --- Update Function --- *
+elif len(sys.argv) > 1 and sys.argv[1] == "update":
+    
+    # try:
+    #     world_c.execute("SELECT name and version from world")
+    
+    # except OperationalError as e:
+    #     print(WorldDatabaseNotBuilded)
+    #     exit()
+
+    tableCompare = "SELECT name, version FROM world WHERE packages='table' order by name"
+    
+    result1 = world_c.execute(tableCompare)
+    result2 = c.execute(tableCompare)
+    
+    print("....Comparing Tables")
+    for row1 in result1:
+        row2 = result2.fetchone()
+        print(row1)
+        print(row2)
+        if row1 is not None and row2 is not None and (row1[0] == row2[0]):
+            print("........Tables Match:"+ row1[0])
+        else:
+            if (row1 is not None and row1[0] is not None):
+                print("!!!!!!!!PROBLEM "+world_database+" is missing Table:" + row1[0])	
+            else:		
+                print("!!!!!!!!PROBLEM "+db+" is missing Table:" + row2[0])	
+            print("........Fix the problem and restart this comparator")
+            exit()
+
+    print("....Done comparing table presence")
+
+
+    
+    result1 = world_c.fetchall()
+    result2 = c.fetchall()
+    
+    for row in result1:
+        if row not in result2:
+            print(row)
+
+        for row in result2:
+            if row not in result1:
+                print(row)
 
 
 # * --- Help Function --- *
