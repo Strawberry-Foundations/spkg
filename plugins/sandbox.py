e2e5af1154fa63e9ac49b09ccda5a56350ed0b03
Julian <81520713+Juliandev02@users.noreply.github.com>
2023-04-20 20:50:31 +0200

    reimplementation of spkg-sandbox now in python

--- plugins/sandbox.py
+++ plugins/sandbox.py
@@ -7,8 +7,17 @@ from sqlite3 import *
 from colorama import Fore
 from halo import Halo
 from sys import exit
-import subprocess
 import os
+import platform
+
+# Define Home Directory
+home_dir = os.getenv("HOME")
+
+# Use User Home even if spkg was executed with sudo
+if os.environ.get('SUDO_USER'):
+    home_dir = os.path.expanduser(f"~{os.environ['SUDO_USER']}")
+else:
+    home_dir = os.path.expanduser("~")
 
 # Language Config
 spkg_config = "/etc/spkg/config.json"
@@ -19,6 +28,18 @@ language = spkg_cfg['language']
 
 if not language == "de" and not language == "en":
     exit()
+    
+# Basic Variables
+bootstrap_location = f"{home_dir}/.local/spkg/sandbox/"
+dist = "jammy"
+
+BOLD = '\033[1m'
+UNDERLINE = '\033[4m'
+RESET = '\033[0m'
+
+
+with open('/etc/os-release') as f:
+    os_info = dict(line.strip().split('=') for line in f if '=' in line)
 
 # Language Strings
 if language == "de":
@@ -35,13 +56,82 @@ class Spec:
     Commands = f"""
     -> setup
     -> config
+    -> remove
+    -> enter
     """
 
 # PluginHandler Main Class
 class PluginHandler:
     def setup():
-        os.system('bash ./spkg-sandbox')
+        print(f"{Fore.YELLOW + BOLD}[!]{Fore.RESET + RESET} Detecting Operating System")
+        print(f"{Fore.GREEN + BOLD}[!]{Fore.RESET + RESET} Detected {os_info['NAME']}")
         
-    
+        if not os.path.exists("/usr/sbin/debootstrap"):
+            print(f"{Fore.RED + BOLD}Error:{Fore.RESET + RESET} spkg-sandbox cannot be executed on your system. Missing dependency 'debootstrap'")
+            exit()
+        
+        if os_info['ID'] == "debian" and os_info['VERSION_ID'] == '"10"':
+            print(f"{Fore.YELLOW + BOLD}Warning:{Fore.RESET + RESET} Your version of debootstrap is outdated and doesn't support to build Ubuntu 22.04 Jammy Jellyfish.")
+            print(f"         Using Focal Fossa (Ubuntu 20.04) build script instead ...")
+            dist = "focal"
+            
+        elif os_info['ID'] == "debian" and os_info['VERSION_ID'] == '"11"':
+            print(f"{Fore.YELLOW + BOLD}Warning:{Fore.RESET + RESET} Your version of debootstrap is outdated and doesn't support to build Ubuntu 22.04 Jammy Jellyfish.")
+            print(f"         Using Focal Fossa (Ubuntu 20.04) build script instead ...")
+            dist = "focal"
+        
+        elif os_info['ID'] == "ubuntu" and os_info['VERSION_ID'] == '"20.04"':
+            print(f"{Fore.YELLOW + BOLD}Warning:{Fore.RESET + RESET} Your version of ubuntu is outdated and the sandbox cannot continue to work with Ubuntu 22.04 Jammy Jellyfish")
+            print(f"         Using Focal Fossa (Ubuntu 20.04) build script instead ...")
+            dist = "focal"
+        
+        else:
+            print(f"{Fore.YELLOW + BOLD}Warning:{Fore.RESET + RESET} Your Linux distrobution has not yet been tested by the spkg developers. It is possible that spkg-sandbox does not work. Please open a GitHub issue if something is not working. ")
+            
+        print(f"{Fore.YELLOW+ BOLD}[!]{Fore.RESET + RESET} Sandbox Setup will now start")
+        
+        try: 
+            ans = input("Do you want to continue? [Y/N]? ")
+            
+        except KeyboardInterrupt: 
+            print("\nAborting ...")
+            exit()
+        
+        if ans != "y" and ans != "Y" and ans != "j" and ans != "J": 
+            print("Aborting ...")
+            exit()
+            
+        print(f"{Fore.YELLOW+ BOLD}[!]{Fore.RESET + RESET} Checking system architecture")
+        arch = platform.machine()
+        
+        if arch == "x86_64":
+            arch = "amd64"
+            repo = "http://archive.ubuntu.com/ubuntu"
+            
+        elif arch == "x86":
+            arch = "i386"
+            repo = "http://archive.ubuntu.com/ubuntu"
+            
+        elif arch == "aarch64":
+            arch = "arm64"
+            repo="http://ports.ubuntu.com/ubuntu-ports"
+        
+        else:
+            print(f"{Fore.RED + BOLD}Error:{Fore.RESET + RESET} spkg-sandbox cannot be executed on your system. Your architecture is currently not supported.")
+            exit()
+            
+        if os.path.exists(bootstrap_location):
+            os.system(f"sudo rm -rf {bootstrap_location}")
+            
+            
+        
     def config():
         print("config")
+        
+    def remove():
+        print(f"{Fore.YELLOW + BOLD}[!]{Fore.RESET + RESET}Removing sandbox ... This can take some time.")
+        os.system(f"sudo rm -rf {bootstrap_location}")
+        exit()
+        
+    def enter():
+        os.system(f'sudo chroot {bootstrap_location}')
