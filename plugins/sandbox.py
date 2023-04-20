72395dcb2f2084b89fcbe6230b3023b8afd94b13
Julian <81520713+Juliandev02@users.noreply.github.com>
2023-04-20 22:39:54 +0200

    Updated distrobution checking

--- plugins/sandbox.py
+++ plugins/sandbox.py
@@ -29,7 +29,7 @@ language = spkg_cfg['language']
 
 if not language == "de" and not language == "en":
     exit()
-    
+
 # Basic Variables
 bootstrap_location = f"{home_dir}/.local/spkg/sandbox/"
 dist = "jammy"
@@ -65,131 +65,163 @@ class Spec:
 class PluginHandler:
     def setup():
         debug = False
-        
+
         if len(argv) > 4 and argv[4] == "--debug" or len(argv) > 4 and argv[4] == "--verbose" or len(argv) > 4 and argv[4] == "-v" or len(argv) > 4 and argv[4] == "--v":
             debug = True
             print(f"{Fore.YELLOW + BOLD}[!]{Fore.RESET + RESET} Enabling Verbose Mode")
-            
+
         print(f"{Fore.YELLOW + BOLD}[!]{Fore.RESET + RESET} Detecting Operating System")
         print(f"{Fore.GREEN + BOLD}[!]{Fore.RESET + RESET} Detected {os_info['NAME']}")
-        
+
         if not os.path.exists("/usr/sbin/debootstrap"):
             print(f"{Fore.RED + BOLD}Error:{Fore.RESET + RESET} spkg-sandbox cannot be executed on your system. Missing dependency 'debootstrap'")
             exit()
-        
+
         if os_info['ID'] == "debian" and os_info['VERSION_ID'] == '"10"':
             print(f"{Fore.YELLOW + BOLD}Warning:{Fore.RESET + RESET} Your version of debootstrap is outdated and doesn't support to build Ubuntu 22.04 Jammy Jellyfish.")
             print(f"         Using Focal Fossa (Ubuntu 20.04) build script instead ...")
             dist = "focal"
-            
+
         elif os_info['ID'] == "debian" and os_info['VERSION_ID'] == '"11"':
             print(f"{Fore.YELLOW + BOLD}Warning:{Fore.RESET + RESET} Your version of debootstrap is outdated and doesn't support to build Ubuntu 22.04 Jammy Jellyfish.")
             print(f"         Using Focal Fossa (Ubuntu 20.04) build script instead ...")
             dist = "focal"
-        
-        elif os_info['ID'] == "ubuntu" and os_info['VERSION_ID'] == '"20.04"':
+
+        elif os_info['ID'] == "ubuntu" or os_info['ID'] == "Ubuntu" and os_info['VERSION_ID'] == '"20.04"':
             print(f"{Fore.YELLOW + BOLD}Warning:{Fore.RESET + RESET} Your version of ubuntu is outdated and the sandbox cannot continue to work with Ubuntu 22.04 Jammy Jellyfish")
             print(f"         Using Focal Fossa (Ubuntu 20.04) build script instead ...")
             dist = "focal"
-        
+            
+        elif os_info['ID'] == "ubuntu" or os_info['ID'] == "Ubuntu" and os_info['VERSION_ID'] == '"22.04"':
+            dist = "jammy"
+
         else:
             print(f"{Fore.YELLOW + BOLD}Warning:{Fore.RESET + RESET} Your Linux distrobution has not yet been tested by the spkg developers. It is possible that spkg-sandbox does not work. Please open a GitHub issue if something is not working. ")
-            
+            dist = "jammy"
+
         print(f"{Fore.YELLOW + BOLD}[!]{Fore.RESET + RESET} Sandbox Setup will now start")
-        
-        try: 
+
+        try:
             ans = input("Do you want to continue? [Y/N]? ")
-            
-        except KeyboardInterrupt: 
+
+        except KeyboardInterrupt:
             print("\nAborting ...")
             exit()
-        
-        if ans != "y" and ans != "Y" and ans != "j" and ans != "J": 
+
+        if ans != "y" and ans != "Y" and ans != "j" and ans != "J":
             print("Aborting ...")
             exit()
-            
-            
+
         print(f"{Fore.YELLOW + BOLD}[!]{Fore.RESET + RESET} Checking system architecture")
         arch = platform.machine()
-        
+
         if arch == "x86_64":
             arch = "amd64"
             repo = "http://archive.ubuntu.com/ubuntu"
-            
+
         elif arch == "x86":
             arch = "i386"
             repo = "http://archive.ubuntu.com/ubuntu"
-            
+
         elif arch == "aarch64":
             arch = "arm64"
-            repo="http://ports.ubuntu.com/ubuntu-ports"
-        
+            repo = "http://ports.ubuntu.com/ubuntu-ports"
+
         else:
             print(f"{Fore.RED + BOLD}Error:{Fore.RESET + RESET} spkg-sandbox cannot be executed on your system. Your architecture is currently not supported.")
             exit()
-            
+
         if os.path.exists(bootstrap_location):
             print(f"{Fore.YELLOW + BOLD}Warning:{Fore.RESET + RESET} You have already a installation of spkg-sandbox. Reinstalling spkg-sandbox ...")
             os.system(f"sudo rm -rf {bootstrap_location}")
-        
+
         start_time = time.time()
-        
+
+
         print(f"{Fore.YELLOW + BOLD}[!]{Fore.RESET + RESET} Bootstrapping your spkg-sandbox ... This could take some time depending on your drive speed and internet speed")
         print(f"{Fore.CYAN + BOLD}[i]{Fore.RESET + RESET} Installing to {bootstrap_location}")
         os.mkdir(bootstrap_location)
-        
-        os.system(f"sudo debootstrap --arch={arch} --variant=minbase --include=wget,ca-certificates,busybox-static {dist} {bootstrap_location} {repo}")
-        
+
+        if debug == True:
+            os.system(f"sudo debootstrap --arch={arch} --variant=minbase --include=wget,ca-certificates,busybox-static {dist} {bootstrap_location} {repo}")
+
+        else:
+            subprocess.run(["sudo", "debootstrap", f"--arch={arch}", "--variant=minbase", "--include=wget,ca-certificates,busybox-static",
+                           f"{dist}", f"{bootstrap_location}", f"{repo}"], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+
+
         print(f"{Fore.YELLOW + BOLD}[!]{Fore.RESET + RESET} Updating your sandbox ...")
-        
         if debug == True:
             os.system(f"sudo chroot {bootstrap_location} apt clean all")
             os.system(f"sudo chroot {bootstrap_location} apt autoclean")
             os.system(f"sudo chroot {bootstrap_location} apt update")
-            
+
         else:
-            subprocess.run(["sudo", "chroot", f"{bootstrap_location}", "apt", "clean", "all"], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-            subprocess.run(["sudo", "chroot", f"{bootstrap_location}", "apt", "autpclean"], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-            subprocess.run(["sudo", "chroot", f"{bootstrap_location}", "apt", "update"], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-        
+            subprocess.run(["sudo", "chroot", f"{bootstrap_location}", "apt", "clean", "all"],
+                           stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+            subprocess.run(["sudo", "chroot", f"{bootstrap_location}", "apt", "autpclean"],
+                           stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+            subprocess.run(["sudo", "chroot", f"{bootstrap_location}", "apt", "update"],
+                           stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+
+
         print(f"{Fore.YELLOW + BOLD}[!]{Fore.RESET + RESET} Modifying /etc/apt/sources.list ({bootstrap_location}) for the best program compability")
         os.system(f"sudo rm {bootstrap_location}/etc/apt/sources.list")
 
         if arch == "x86_64" or arch == "x86":
-            os.system(f'sudo tee -a {bootstrap_location}/etc/apt/sources.list <<< "deb http://archive.ubuntu.com/ubuntu {dist} main restricted universe multiverse"')
-            os.system(f'sudo tee -a {bootstrap_location}/etc/apt/sources.list <<< "deb http://archive.ubuntu.com/ubuntu {dist}-backports main restricted universe multiverse"')
-            os.system(f'sudo tee -a {bootstrap_location}/etc/apt/sources.list <<< "deb http://archive.ubuntu.com/ubuntu {dist}-proposed main restricted universe multiverse"')
-            os.system(f'sudo tee -a {bootstrap_location}/etc/apt/sources.list <<< "deb http://archive.ubuntu.com/ubuntu {dist}-security main restricted universe multiverse"')
-            os.system(f'sudo tee -a {bootstrap_location}/etc/apt/sources.list <<< "deb http://archive.ubuntu.com/ubuntu {dist}-updates main restricted universe multiverse"')
-            
-        else:   
-            os.system(f"sudo sh -c 'echo deb http://ports.ubuntu.com/ubuntu-ports {dist} main restricted universe multiverse >> {bootstrap_location}/etc/apt/sources.list'")
-            os.system(f"sudo sh -c 'echo deb http://ports.ubuntu.com/ubuntu-ports {dist}-backports main restricted universe multiverse >> {bootstrap_location}/etc/apt/sources.list'")
-            os.system(f"sudo sh -c 'echo deb http://ports.ubuntu.com/ubuntu-ports {dist}-proposed main restricted universe multiverse >> {bootstrap_location}/etc/apt/sources.list'")
-            os.system(f"sudo sh -c 'echo deb http://ports.ubuntu.com/ubuntu-ports {dist}-security main restricted universe multiverse >> {bootstrap_location}/etc/apt/sources.list'")
-            os.system(f"sudo sh -c 'echo deb http://ports.ubuntu.com/ubuntu-ports {dist}-updates main restricted universe multiverse >> {bootstrap_location}/etc/apt/sources.list'")
-            
+            os.system(
+                f'sudo tee -a {bootstrap_location}/etc/apt/sources.list <<< "deb http://archive.ubuntu.com/ubuntu {dist} main restricted universe multiverse"')
+            os.system(
+                f'sudo tee -a {bootstrap_location}/etc/apt/sources.list <<< "deb http://archive.ubuntu.com/ubuntu {dist}-backports main restricted universe multiverse"')
+            os.system(
+                f'sudo tee -a {bootstrap_location}/etc/apt/sources.list <<< "deb http://archive.ubuntu.com/ubuntu {dist}-proposed main restricted universe multiverse"')
+            os.system(
+                f'sudo tee -a {bootstrap_location}/etc/apt/sources.list <<< "deb http://archive.ubuntu.com/ubuntu {dist}-security main restricted universe multiverse"')
+            os.system(
+                f'sudo tee -a {bootstrap_location}/etc/apt/sources.list <<< "deb http://archive.ubuntu.com/ubuntu {dist}-updates main restricted universe multiverse"')
+
+        else:
+            os.system(
+                f"sudo sh -c 'echo deb http://ports.ubuntu.com/ubuntu-ports {dist} main restricted universe multiverse >> {bootstrap_location}/etc/apt/sources.list'")
+            os.system(
+                f"sudo sh -c 'echo deb http://ports.ubuntu.com/ubuntu-ports {dist}-backports main restricted universe multiverse >> {bootstrap_location}/etc/apt/sources.list'")
+            os.system(
+                f"sudo sh -c 'echo deb http://ports.ubuntu.com/ubuntu-ports {dist}-proposed main restricted universe multiverse >> {bootstrap_location}/etc/apt/sources.list'")
+            os.system(
+                f"sudo sh -c 'echo deb http://ports.ubuntu.com/ubuntu-ports {dist}-security main restricted universe multiverse >> {bootstrap_location}/etc/apt/sources.list'")
+            os.system(
+                f"sudo sh -c 'echo deb http://ports.ubuntu.com/ubuntu-ports {dist}-updates main restricted universe multiverse >> {bootstrap_location}/etc/apt/sources.list'")
+
+
         print(f"{Fore.YELLOW + BOLD}[!]{Fore.RESET + RESET} Installing some base packages ... ")
         if debug == True:
             os.system(f"sudo chroot {bootstrap_location} apt update")
             os.system(f"sudo chroot {bootstrap_location} apt install -y python3 python3-dev python3-pip")
+            print(f"{Fore.YELLOW + BOLD}[!]{Fore.RESET + RESET} Upgrading your sandbox ... ")
             os.system(f"sudo chroot {bootstrap_location} apt upgrade -y")
+            
         else:
-            subprocess.run(["sudo", "chroot", f"{bootstrap_location}", "apt", "update"], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-            os.system(f"sudo chroot {bootstrap_location} apt install -y python3 python3-dev python3-pip")
-            subprocess.run(["sudo", "chroot", f"{bootstrap_location}", "apt", "upgrade"], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-        
+            subprocess.run(["sudo", "chroot", f"{bootstrap_location}", "apt", "update"],
+                           stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+            subprocess.run(["sudo", "chroot", f"{bootstrap_location}", "apt", "install", "-y", "python3", "python3-dev", "python3-pip"],
+                           stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+            print(f"{Fore.YELLOW + BOLD}[!]{Fore.RESET + RESET} Upgrading your sandbox ... ")
+            subprocess.run(["sudo", "chroot", f"{bootstrap_location}", "apt", "upgrade", "-y"],
+                           stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+
         end_time = time.time()
-        
-        print(f"{Fore.GREEN + BOLD}[!]{Fore.RESET + RESET} Finished sandbox setup in {round(end_time - start_time, 2)} s")
-        
+
+        print(
+            f"{Fore.GREEN + BOLD}[!]{Fore.RESET + RESET} Finished sandbox setup in {round(end_time - start_time, 2)} s")
+
     def config():
         print("config")
-        
+
     def remove():
-        print(f"{Fore.YELLOW + BOLD}[!]{Fore.RESET + RESET}Removing sandbox ... This can take some time.")
+        print(
+            f"{Fore.YELLOW + BOLD}[!]{Fore.RESET + RESET}Removing sandbox ... This can take some time.")
         os.system(f"sudo rm -rf {bootstrap_location}")
         exit()
-        
+
     def enter():
         os.system(f'sudo chroot {bootstrap_location}')
