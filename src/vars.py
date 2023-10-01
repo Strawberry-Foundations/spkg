c27802e2dcf10807cd498e503ad54bcc59b5f4fa
Juliandev02 <juli.mackenstein@gmail.com>
2023-10-01 21:40:56 +0200

    Updated vars

--- src/vars.py
+++ src/vars.py
@@ -1,13 +1,17 @@
 # Base Variables
-base_version    = "2.0.0"
-version         = "2.0a1+u1"
-date            = "20230925"
-release_type    = "alpha"
-alpha           = True
-dev_local       = True
-langs           = ["de_DE", "en_US"]
+base_version            = "2.0"
+version                 = "2.0a1"
+full_ver                = "v" + version
+update_channel          = "dev"
+date                    = "20230925"
+ver                     = version + "-vacakes"
+codename                = "Vanilla Cake"
+authors                 = ["Juliandev02", "matteodev8", "Paddyk45"]
+api                     = "http://api.strawberryfoundations.xyz/v1/"
+langs                   = ["de_DE", "en_US"]
+dev_local               = True
 
-if release_type in ["rc", "beta", "alpha"]:
+if update_channel in ["dev", "beta", "alpha"]:
     version = version + f" ({date})"
     
 else: 
