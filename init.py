8fce26c93cb0302451f7b0409be8da3ab346b07e
Juliandev02 <juli.mackenstein@gmail.com>
2023-10-01 18:31:14 +0200

    moved some entries to src.vars

--- init.py
+++ init.py
@@ -27,21 +27,7 @@ import yaml
 from yaml import SafeLoader
 from src.colors import *
 from src.arch import ARCH
-
-# Base Variables
-base_version    = "2.0.0"
-version         = "2.0a1+u1"
-date            = "20230925"
-release_type    = "alpha"
-alpha           = True
-dev_local       = True
-langs           = ["de_DE", "en_US"]
-
-if release_type in ["rc", "beta", "alpha"]:
-    version = version + f" ({date})"
-    
-else: 
-    version = version
+from src.vars import dev_local
 
 # Environ Variables
 home_dir = os.getenv("HOME")
