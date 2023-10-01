61ba8e2aff20aebf76bee121acade6c9f9e588ef
Juliandev02 <juli.mackenstein@gmail.com>
2023-10-01 18:27:38 +0200

    Updated database wrapper

--- src/db.py
+++ src/db.py
@@ -3,28 +3,34 @@ import os
 import sys
 from init import * 
 
+class Database:
+    def __init__(self, database, check_same_thread: bool = False):
+        self.db_connection = sql.connect(database, check_same_thread)
+        self.cursor = self.db_connection.cursor()
 
-class StbWrapper:
-    class DB:
-        def __init__(self, file):
-            self.db_connection = sql.connect(file, check_same_thread=False)
-            self.cursor = self.db_connection.cursor()
+    def execute(self, statement, __parameters = ()):
+        self.cursor.execute(statement, __parameters)
+    
+    def commit(self):
+        self.db_connection.commit()
+    
+    def close(self):
+        self.cursor.close()
+    
+    def cursor_close(self):
+        self.db_connection.close()
+        
+    def fetchone(self):
+        return self.cursor.fetchone()
 
-        def execute(self, statement):
-            self.db_connection.execute(statement)
-        
-        def commit(self):
-            self.db_connection.commit()
-        
-        def close(self):
-            self.db_connection.close()
-            self.cursor.close()
+    def fetchall(self):
+        return self.cursor.fetchall()
             
 
 class Tools:
     def regen_world():
         os.remove(Files.world_database)
-        db = StbWrapper.DB(Files.world_database)
+        db = Database(Files.world_database)
         db.execute(SQLDatabase.World.query)
         db.commit()
         db.close()
