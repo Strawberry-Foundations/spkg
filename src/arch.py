f20ca916d997faa3a98f4b7d1ce16f0b00cba9c8
Juliandev02 <juli.mackenstein@gmail.com>
2023-10-02 16:13:00 +0200

    Added iOS ARM64(e) architecture support

--- src/arch.py
+++ src/arch.py
@@ -1,4 +1,6 @@
 class ARCH: 
-    X86_64  = "amd64"
-    X86     = "i386"
-    AARCH64 = "arm64"
+    X86_64      = "amd64"
+    X86         = "i386"
+    AARCH64     = "arm64"
+    IOS_ARM64   = "ios-arm64"
+    IOS_ARM64E  = "ios-arm64e"
