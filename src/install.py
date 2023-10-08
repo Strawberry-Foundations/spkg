e6f28d2db37b374da65adce1bd59c6defd69bf71
Juliandev02 <juli.mackenstein@gmail.com>
2023-10-08 21:55:31 +0200

    (i think) FINALLY FIXED INSTALL COMMAND!

--- src/install.py
+++ src/install.py
@@ -501,24 +501,17 @@ class InstallManager:
                         shell = subprocess.Popen(['/bin/bash'], stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
                             
                         for command in install_command:
-                            # subprocess.run(command, shell=True, check=True, text=True)
-                            # os.system(command)
                             shell.stdin.write(command + '\n')
                             shell.stdin.flush()
-                        
-                        # Lies die Ausgabe der Shell (könnte stdout und stderr sein)
+                            
                         output, errors = shell.communicate()
 
-                        # Gib die Ausgabe aus
-                        print("Ausgabe:")
-                        print(output)
-
+                        # print(output)
 
                         if not errors.rstrip() == "":
                             print(StringLoader("EncounteredErrors"))
                             print(errors)
 
-                        # Beende die Shell
                         shell.terminate()
 
                     except Exception as e: 
