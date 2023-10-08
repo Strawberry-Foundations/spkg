25c3c7e6864c5085151cbc6a5fb5204be0260e44
Juliandev02 <juli.mackenstein@gmail.com>
2023-10-08 18:03:53 +0200

    updated basic yml

--- yaml_pkg/yaml_basic.py
+++ yaml_pkg/yaml_basic.py
@@ -2,3 +2,22 @@ import yaml
 from yaml import SafeLoader
 
 import subprocess
+import os
+
+with open("./test.yml") as file:
+    package = yaml.load(file, Loader=SafeLoader)
+    
+print(f'''
+      Name: {package["Name"]}
+      Version: {package["Version"]} 
+      Architecture: {package["Architecture"]}
+      Dependencies: {package["Dependencies"]}
+      ''')
+
+SpecName        = package["Name"]
+SpecVersion     = package["Version"]
+SpecArch        = package["Architecture"]
+SpecDeps        = package["Dependencies"]
+SpecFlags       = package["Flags"]
+
+os.system(package["Install"]["Commands"])
