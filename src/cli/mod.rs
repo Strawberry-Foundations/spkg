f266a9248aa5899ef3e4d0dba44ca5ae9360c2b8
Juliandev02 <juli.mackenstein@gmail.com>
2024-01-15 15:03:57 +0000

    Refactoring

--- src/cli/mod.rs
+++ src/cli/mod.rs
@@ -1,1 +1,8 @@
 pub mod args;
+
+use lazy_static::lazy_static;
+
+lazy_static!(
+    pub static ref ARGS: args::Args = args::Args::collect();
+    pub static ref SPKG_OPTIONS: args::SpkgOptions = args::Args::collect().collect_options();
+);
