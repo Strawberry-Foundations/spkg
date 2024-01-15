a5795db0fecdb965382b56bc8928244f988b2919
Juliandev02 <juli.mackenstein@gmail.com>
2024-01-15 15:38:04 +0000

    Refactoring

--- src/spkg_core/mod.rs
+++ src/spkg_core/mod.rs
@@ -1,9 +1,13 @@
-pub mod config;
+pub mod files;
 pub mod types;
+pub mod config;
 
 use lazy_static::lazy_static;
+use stblib::strings::Strings;
 
 lazy_static!(
-    pub static ref SPKG_DIRECTORIES: config::SpkgDirectories = config::SpkgDirectories::new();
-    pub static ref SPKG_FILES: config::SpkgFiles = config::SpkgFiles::new();
+    pub static ref SPKG_DIRECTORIES: files::SpkgDirectories = files::SpkgDirectories::new();
+    pub static ref SPKG_FILES: files::SpkgFiles = files::SpkgFiles::new();
+
+    pub static ref STRING_LOADER: Strings = Strings::new();
 );
