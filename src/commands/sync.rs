3e22850b253dd366da8230149dd44ad2936ac61c
Juliandev02 <juli.mackenstein@gmail.com>
2024-01-15 18:43:14 +0000

    some tests

--- src/commands/sync.rs
+++ src/commands/sync.rs
@@ -1,24 +1,40 @@
 use std::fs::File;
 use std::io::copy;
+use std::sync::{Arc, Mutex};
 use std::time::Instant;
 
-use stblib::colors::{BOLD, C_RESET, CYAN};
+use stblib::colors::{BOLD, C_RESET, CYAN, GREEN, RED};
 
 use crate::spkg_core::{CONFIG, SPKG_DIRECTORIES, STRING_LOADER};
+use crate::utilities::delete_last_line;
 
 pub fn sync() {
     let start_time = Instant::now();
 
+    let success_counter = Arc::new(Mutex::new(0));
+    let unsuccess_counter = Arc::new(Mutex::new(0));
+
     for (name, url) in CONFIG.repositories.iter() {
+        let unsuccess_counter_clone = unsuccess_counter.clone();
+
         let database_repo = format!("{url}/package.db");
         let database_local = format!("{}{name}.db", SPKG_DIRECTORIES.mirrors);
 
-        println!("{} {CYAN}{BOLD}{url}{C_RESET} ({name}) ...{C_RESET}", STRING_LOADER.str("SyncingPackageDatabase"));
+        println!("... {} {CYAN}{BOLD}{url}{C_RESET} ({name}) ...{C_RESET}", STRING_LOADER.str("SyncingPackageDatabase"));
 
-        let mut response = reqwest::blocking::get(database_repo).unwrap_or_else(|_| {
-            eprintln!("{}", STRING_LOADER.str("HttpError"));
-            std::process::exit(1);
-        });
+        std::thread::sleep(std::time::Duration::from_secs(2));
+
+        let Ok(mut response) = reqwest::blocking::get(database_repo.clone()) else {
+            delete_last_line();
+
+            eprintln!("{RED}{BOLD}[×]{C_RESET} {} {CYAN}{BOLD}{url}{C_RESET} ({name}) ...{C_RESET}", STRING_LOADER.str("SyncingPackageDatabase"));
+            eprintln!("{RED}{BOLD} ↳  {}{C_RESET}", STRING_LOADER.str("HttpError"));
+
+            let mut counter = unsuccess_counter_clone.lock().unwrap();
+            *counter += 1;
+
+            continue
+        };
 
         if response.status().is_success() {
             let mut database = File::create(database_local).unwrap_or_else(|_| {
@@ -35,9 +51,18 @@ pub fn sync() {
 
         }
         else {
-            eprintln!("{}", STRING_LOADER.str("HttpError"));
+            eprintln!("{}{C_RESET}", STRING_LOADER.str("HttpError"));
         }
+
+        delete_last_line();
+        println!("{GREEN}{BOLD}[✓]{C_RESET} {} {CYAN}{BOLD}{url}{C_RESET} ({name}) ...{C_RESET}", STRING_LOADER.str("SyncingPackageDatabase"));
+
+        let mut counter = success_counter.lock().unwrap();
+        *counter += 1;
     }
+
+    println!("{}", success_counter.lock().unwrap());
+    println!("{}", unsuccess_counter.lock().unwrap());
 
     println!("{}", STRING_LOADER.str_params("SuccessSyncingPackageDatabase", &[&format!("{:.2}", start_time.elapsed().as_secs_f64())]));
 }
