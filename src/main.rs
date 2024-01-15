1636efbcf6f2ccb4f2e2ff3da854f649780d5da0
Juliandev02 <juli.mackenstein@gmail.com>
2024-01-15 15:51:09 +0100

    updates

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,12 @@
 use crate::cli::args::Args;
+use crate::global::SPKG_OPTIONS;
+
 mod cli;
+mod global;
 
 fn main() {
     let mut args = Args::new();
     args.collect();
 
-    println!("{}", args.command);
-    println!("{:?}", args.options);
+    println!("{}", SPKG_OPTIONS.sandbox);
 }
