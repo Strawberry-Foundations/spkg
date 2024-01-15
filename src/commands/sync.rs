bdf5e7c219f8387cee99c3bab93e907e3cdc4c65
Juliandev02 <juli.mackenstein@gmail.com>
2024-01-15 17:51:01 +0000

    added file downloading

--- src/commands/sync.rs
+++ src/commands/sync.rs
@@ -1,9 +1,41 @@
-use crate::spkg_core::CONFIG;
+use std::fs::File;
+use std::io::copy;
+
+use stblib::colors::{BOLD, C_RESET, CYAN};
+
+use crate::spkg_core::{CONFIG, SPKG_DIRECTORIES, STRING_LOADER};
 
 pub fn sync() {
     for (name, url) in CONFIG.repositories.iter() {
-        let repo = format!("{url}/package.db");
+        let database_repo = format!("{url}/package.db");
+        let database_local = format!("{}{name}.db", SPKG_DIRECTORIES.mirrors);
 
-        println!("{repo}");
+        println!("{} {CYAN}{BOLD}{url}{C_RESET} ({name}) ...{C_RESET}", STRING_LOADER.str("SyncingPackageDatabase"));
+
+        let mut response = reqwest::blocking::get(database_repo).unwrap_or_else(|_| {
+            eprintln!("{}", STRING_LOADER.str("HttpError"));
+            std::process::exit(1)
+        });
+
+        if response.status().is_success() {
+            let mut database = File::create(database_local).unwrap_or_else(|_| {
+                eprintln!("{CYAN}{BOLD}{}:{C_RESET} {}", SPKG_DIRECTORIES.mirrors, STRING_LOADER.str("MissingPermissions"));
+                eprintln!("{}", STRING_LOADER.str("MissingPermissionsPackageDatabaseUpdate"));
+                std::process::exit(1);
+            });
+
+            copy(&mut response, &mut database).unwrap_or_else(|_| {
+                eprintln!("{CYAN}{BOLD}{}:{C_RESET} {}", SPKG_DIRECTORIES.mirrors, STRING_LOADER.str("MissingPermissions"));
+                eprintln!("{}", STRING_LOADER.str("MissingPermissionsPackageDatabaseUpdate"));
+                std::process::exit(1);
+            });
+
+        }
+        else {
+            eprintln!("{}", STRING_LOADER.str("HttpError"));
+            std::process::exit(1)
+        }
     }
+
+    println!("{}", STRING_LOADER.str("SuccessSyncingPackageDatabase"));
 }
