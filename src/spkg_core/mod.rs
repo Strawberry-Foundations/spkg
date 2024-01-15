cec394292117b817f47b220fe435898e8a872dba
Juliandev02 <juli.mackenstein@gmail.com>
2024-01-15 15:53:22 +0000

    Some path fixes

--- src/spkg_core/mod.rs
+++ src/spkg_core/mod.rs
@@ -4,11 +4,12 @@ pub mod config;
 
 use lazy_static::lazy_static;
 use stblib::strings::Strings;
+use crate::spkg_core::config::get_language_strings;
 
 lazy_static!(
     pub static ref CONFIG: config::Config = config::Config::new();
     pub static ref SPKG_DIRECTORIES: files::SpkgDirectories = files::SpkgDirectories::new();
     pub static ref SPKG_FILES: files::SpkgFiles = files::SpkgFiles::new();
 
-    pub static ref STRING_LOADER: Strings = Strings::new(&CONFIG.language, &CONFIG.language_strings);
+    pub static ref STRING_LOADER: Strings = Strings::new(&CONFIG.language, get_language_strings().as_str());
 );
