92c75d9f6a2109c42bfe8ed01370a40d51cfdb6e
Juliandev02 <juli.mackenstein@gmail.com>
2024-01-15 15:03:54 +0000

    Refactoring

--- src/global.rs
+++
@@ -1,8 +0,0 @@
-use lazy_static::lazy_static;
-
-use crate::cli::args::{Args, SpkgOptions};
-
-lazy_static!(
-    pub static ref ARGS: Args = Args::collect();
-    pub static ref SPKG_OPTIONS: SpkgOptions = Args::collect().collect_options();
-);
