e2d60abccb21a38e451dfb026d20ed17a4c5ab6c
Juliandev02 <juli.mackenstein@gmail.com>
2024-01-15 15:53:17 +0000

    Some path fixes

--- src/spkg_core/config.rs
+++ src/spkg_core/config.rs
@@ -1,21 +1,22 @@
 use serde::Deserialize;
 use serde_yaml::from_str;
 
-use crate::spkg_core::{SPKG_DIRECTORIES, SPKG_FILES};
-use crate::utilities::open_file;
+use crate::spkg_core::{CONFIG, SPKG_DIRECTORIES, SPKG_FILES};
+use crate::utilities::{open_file, open_file_2};
 
 #[derive(Debug, Deserialize)]
 pub struct Config {
     pub language: String,
-    pub language_strings: String,
+}
+
+pub fn get_language_strings() -> String {
+    open_file_2(format!("{}{}.yml", &SPKG_DIRECTORIES.language_files, CONFIG.language).as_str())
 }
 
 impl Config {
     pub fn new() -> Self {
         let system_config_raw = open_file(&SPKG_FILES.system_config);
         let mut config: Self = from_str(&system_config_raw).unwrap();
-
-        config.language_strings = open_file(format!("{}{}.yml", &SPKG_DIRECTORIES.language_files, config.language).as_str());
 
         config
     }
