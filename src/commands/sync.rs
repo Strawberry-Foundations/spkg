0c9ff313a21f087a7a0667474c8f538d7cb31247
Juliandev02 <juli.mackenstein@gmail.com>
2024-01-15 17:30:04 +0000

    basic iterator

--- src/commands/sync.rs
+++ src/commands/sync.rs
@@ -1,3 +1,9 @@
-pub fn sync() {
+use crate::spkg_core::CONFIG;
 
+pub fn sync() {
+    for (name, url) in CONFIG.repositories.iter() {
+        let repo = format!("{url}/package.db");
+
+        println!("{repo}");
+    }
 }
