5b1323730634d6946f47c3dd3ed9dcb8a38c75d7
Juliandev02 <juli.mackenstein@gmail.com>
2024-01-15 15:47:43 +0000

    Added STRING_LOADER

--- src/spkg_core/config.rs
+++ src/spkg_core/config.rs
@@ -1,18 +1,21 @@
 use serde::Deserialize;
 use serde_yaml::from_str;
 
-use crate::spkg_core::SPKG_FILES;
+use crate::spkg_core::{SPKG_DIRECTORIES, SPKG_FILES};
 use crate::utilities::open_file;
 
 #[derive(Debug, Deserialize)]
 pub struct Config {
     pub language: String,
+    pub language_strings: String,
 }
 
 impl Config {
     pub fn new() -> Self {
         let system_config_raw = open_file(&SPKG_FILES.system_config);
         let mut config: Self = from_str(&system_config_raw).unwrap();
+
+        config.language_strings = open_file(format!("{}{}.yml", &SPKG_DIRECTORIES.language_files, config.language).as_str());
 
         config
     }
