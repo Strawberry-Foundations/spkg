b96c0433f9e52cbab1964e9fa328c9310df572d7
Juliandev02 <juli.mackenstein@gmail.com>
2024-01-15 18:16:30 +0000

    Added delete_last_line

--- src/utilities.rs
+++ src/utilities.rs
@@ -1,5 +1,12 @@
-use std::fs;
+use std::{fs, io};
+use std::io::Write;
 
 pub fn open_file(path: &str) -> String {
     fs::read_to_string(path).expect("Could not read file")
+}
+
+pub fn delete_last_line() {
+    print!("\x1b[1A");
+    print!("\x1b[2K");
+    io::stdout().flush().unwrap();
 }
