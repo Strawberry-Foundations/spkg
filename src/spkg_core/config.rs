56b3a888b8652f4dcc57c9a4acad16a4706c6785
Juliandev02 <juli.mackenstein@gmail.com>
2024-01-15 15:53:58 +0000

    removed mutable state of variale

--- src/spkg_core/config.rs
+++ src/spkg_core/config.rs
@@ -2,7 +2,7 @@ use serde::Deserialize;
 use serde_yaml::from_str;
 
 use crate::spkg_core::{CONFIG, SPKG_DIRECTORIES, SPKG_FILES};
-use crate::utilities::{open_file, open_file_2};
+use crate::utilities::open_file;
 
 #[derive(Debug, Deserialize)]
 pub struct Config {
@@ -10,13 +10,13 @@ pub struct Config {
 }
 
 pub fn get_language_strings() -> String {
-    open_file_2(format!("{}{}.yml", &SPKG_DIRECTORIES.language_files, CONFIG.language).as_str())
+    open_file(format!("{}{}.yml", &SPKG_DIRECTORIES.language_files, CONFIG.language).as_str())
 }
 
 impl Config {
     pub fn new() -> Self {
         let system_config_raw = open_file(&SPKG_FILES.system_config);
-        let mut config: Self = from_str(&system_config_raw).unwrap();
+        let config: Self = from_str(&system_config_raw).unwrap();
 
         config
     }
