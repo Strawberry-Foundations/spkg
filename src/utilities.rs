a7d6b5ac8834593e8eec6fe20338c5bb93e2b75b
Juliandev02 <juli.mackenstein@gmail.com>
2024-01-15 15:53:23 +0000

    Some path fixes

--- src/utilities.rs
+++ src/utilities.rs
@@ -3,3 +3,7 @@ use std::fs;
 pub fn open_file(path: &str) -> String {
     fs::read_to_string(path).expect("Could not read file")
 }
+
+pub fn open_file_2(path: &str) -> String {
+    fs::read_to_string(path).expect("Could not read file 2")
+}
