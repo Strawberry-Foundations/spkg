1d7db1059a889b4200307e9ad4fa25b2fb12df59
Juliandev02 <juli.mackenstein@gmail.com>
2024-01-18 19:53:56 +0000

    added world database builder

---
+++ src/commands/build.rs
@@ -0,0 +1,43 @@
+use std::process::exit;
+use std::time::Duration;
+
+use rustyline::error::ReadlineError;
+use stblib::colors::{BOLD, C_RESET, CYAN};
+use tokio::time::sleep;
+use crate::cli::SPKG_OPTIONS;
+use crate::db::db::Database;
+use crate::spkg_core::{SPKG_FILES, STRING_LOADER};
+
+pub async fn build() {
+    if SPKG_OPTIONS.build_world {
+        let mut line_reader = rustyline::DefaultEditor::new().unwrap();
+
+        let input: String = match line_reader.readline(&STRING_LOADER.str("AskRegenWorld")) {
+            Ok(i) => i,
+            Err(ReadlineError::Interrupted) => {
+                sleep(Duration::from_millis(300)).await;
+                exit(0);
+            }
+            Err(ReadlineError::Eof) => exit(0),
+            Err(_) => exit(1),
+        };
+
+        if input.to_lowercase() == "yes" || input.to_lowercase() == "ja" {
+            println!("{C_RESET}{}", STRING_LOADER.str("BuildingWorldDatabase"));
+
+            std::fs::remove_file(&SPKG_FILES.world_database).unwrap_or_else(|_| {
+                eprintln!("{CYAN}{BOLD}{}:{C_RESET} {}", SPKG_FILES.world_database, STRING_LOADER.str("MissingPermissions"));
+                eprintln!("{}", STRING_LOADER.str("MissingPermissionsWorldDatabase"));
+                exit(1);
+            });
+
+
+            let db = Database::new(&SPKG_FILES.world_database).await;
+
+            sqlx::query(r#"CREATE TABLE "world" ("name" TEXT, "version" INTEGER, "branch" TEXT, "arch" TEXT)"#)
+                .execute(&db.connection)
+                .await.unwrap();
+
+        }
+    }
+}
