bd83678b0b78f7f4c6dd96e2ee986ab4c27981f5
Juliandev02 <juli.mackenstein@gmail.com>
2024-01-18 19:07:15 +0000

    error handling

--- src/db/db.rs
+++ src/db/db.rs
@@ -1,5 +1,7 @@
 use sqlx::{Pool, Sqlite};
 use sqlx::sqlite::SqlitePool;
+use stblib::colors::{BOLD, C_RESET, YELLOW};
+use crate::spkg_core::{SPKG_FILES, STRING_LOADER};
 
 pub struct Database {
     pub connection: Pool<Sqlite>,
@@ -9,7 +11,12 @@ pub struct Database {
 impl Database {
     pub async fn new(path: impl ToString) -> Self {
         let sqlite_db = SqlitePool::connect(&path.to_string()).await.unwrap_or_else(|_| {
-            eprintln!("sqlite::connection_error");
+            if path.to_string() == SPKG_FILES.world_database {
+                eprintln!("{YELLOW}{BOLD} ! {C_RESET}{}{C_RESET}", STRING_LOADER.str("WorldDatabaseNotBuilt"));
+                std::process::exit(1);
+            }
+
+            eprintln!("{}", STRING_LOADER.str("PackageDatabaseNotSynced"));
             std::process::exit(1);
         });
 
