18b9923c2a31246cf7d021912f20c7b4a9f19fbc
Juliandev02 <juli.mackenstein@gmail.com>
2024-01-18 18:58:58 +0000

    Added arch option

--- src/commands/list.rs
+++ src/commands/list.rs
@@ -1,14 +1,27 @@
 use sqlx::Row;
 use stblib::colors::{BOLD, C_RESET, CYAN, GREEN, RESET};
+use crate::cli::SPKG_OPTIONS;
 use crate::db::db::Database;
 use crate::spkg_core::SPKG_FILES;
 
 pub async fn list() {
-    let db = Database::new(&SPKG_FILES.package_database).await;
+    let db = if SPKG_OPTIONS.list_installed {
+        Database::new(&SPKG_FILES.world_database).await
+    }
+    else {
+        Database::new(&SPKG_FILES.package_database).await
+    };
 
-    let entries = sqlx::query("SELECT * FROM packages ORDER BY name GLOB '[A-Za-z]*' DESC, name")
-        .fetch_all(&db.connection)
-        .await.unwrap();
+    let entries = if SPKG_OPTIONS.list_custom_arch {
+        sqlx::query("SELECT * FROM packages WHERE arch = ? ORDER BY name GLOB '[A-Za-z]*' DESC, name")
+            .bind(&SPKG_OPTIONS.list_custom_arch_type)
+            .fetch_all(&db.connection)
+            .await.unwrap()
+    } else {
+        sqlx::query("SELECT * FROM packages ORDER BY name GLOB '[A-Za-z]*' DESC, name")
+            .fetch_all(&db.connection)
+            .await.unwrap()
+    };
 
     for entry in entries {
         let name: String = entry.get("name");
