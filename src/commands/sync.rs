2fe4c23e9446582a7daa2d8237bd37ac9a27a951
Juliandev02 <julian@strawberryfoundations.xyz>
2024-05-10 18:55:41 +0200

    fixed sync command

--- src/commands/sync.rs
+++ src/commands/sync.rs
@@ -1,14 +1,16 @@
 use std::sync::{Arc, Mutex};
-use std::time::Instant;
+use std::time::{Instant};
 
 use tokio::fs::File;
 use tokio::io::AsyncWriteExt;
 
 use futures_util::stream::StreamExt;
+
+use stblib::colors::{BOLD, C_RESET, CYAN, GREEN, YELLOW};
+
+use crate::err;
+use crate::err::SpkgError;
 use crate::spinners::{Spinner, Spinners};
-
-use stblib::colors::{BOLD, C_RESET, CYAN, GREEN, RED, YELLOW};
-
 use crate::fs::format::format_size;
 use crate::net::remote::remote_header;
 use crate::spkg_core::{CONFIG, SPKG_DIRECTORIES, STRING_LOADER};
@@ -27,15 +29,16 @@ pub async fn sync() {
         let database_local = format!("{}{name}.db", SPKG_DIRECTORIES.mirrors);
         let content_size = remote_header(&database_repo).await;
 
-        let mut sp = Spinner::new(Spinners::Line, format!("{} {CYAN}{BOLD}{url}{C_RESET} ({name}) ...{C_RESET}", STRING_LOADER.load("SyncingPackageDatabase")));
-
-        // println!("... {} {CYAN}{BOLD}{url}{C_RESET} ({name}) ...{C_RESET}", STRING_LOADER.load("SyncingPackageDatabase"));
+        let mut sp = Spinner::new(
+            Spinners::Line, format!(
+                "{} {CYAN}{BOLD}{url}{C_RESET} ({name}) ...{C_RESET}",
+                STRING_LOADER.load("SyncingPackageDatabase"))
+        );
 
         let Ok(response) = reqwest::get(database_repo.clone()).await else {
+            sp.stop_with_message(String::new());
             delete_last_line();
-
-            eprintln!("{RED}{BOLD} × {C_RESET} {} {CYAN}{BOLD}{url}{C_RESET} ({name}) ...{C_RESET}", STRING_LOADER.load("SyncingPackageDatabase"));
-            eprintln!("{RED}{BOLD} ↳  {}{C_RESET}", STRING_LOADER.load("HttpError"));
+            err::throw(SpkgError::HttpError, &[url, name]);
 
             let mut counter = failed_counter_clone.lock().unwrap();
             *counter += 1;
@@ -45,8 +48,9 @@ pub async fn sync() {
 
         if response.status().is_success() {
             let mut database = File::create(database_local).await.unwrap_or_else(|_| {
-                eprintln!("{CYAN}{BOLD}{}:{C_RESET} {}", SPKG_DIRECTORIES.mirrors, STRING_LOADER.load("MissingPermissions"));
-                eprintln!("{}", STRING_LOADER.load("MissingPermissionsPackageDatabaseUpdate"));
+                sp.stop_with_message(String::new());
+                delete_last_line();
+                err::throw(SpkgError::MissingPermissions("MissingPermissionsPackageDatabaseUpdate"), &[]);
                 std::process::exit(1);
             });
 
@@ -56,34 +60,34 @@ pub async fn sync() {
                 let chunk = item;
 
                 database.write_all(&chunk.unwrap()).await.unwrap_or_else(|_| {
-                    eprintln!("{CYAN}{BOLD}{}:{C_RESET} {}", SPKG_DIRECTORIES.mirrors, STRING_LOADER.load("MissingPermissions"));
-                    eprintln!("{}", STRING_LOADER.load("MissingPermissionsPackageDatabaseUpdate"));
+                    sp.stop_with_message(String::new());
+                    delete_last_line();
+                    err::throw(SpkgError::MissingPermissions("MissingPermissionsPackageDatabaseUpdate"), &[]);
                     std::process::exit(1);
                 });
             }
         }
 
         else {
-            eprintln!("{}{C_RESET}", STRING_LOADER.load("HttpError"));
+            sp.stop_with_message(String::new());
+            delete_last_line();
+            err::throw(SpkgError::HttpError, &[url, name]);
+
+            let mut counter = failed_counter_clone.lock().unwrap();
+            *counter += 1;
+            
+            continue;
         }
 
-        sp.stop_with_message(format!("{GREEN}{BOLD} ✓ {C_RESET} {} {CYAN}{BOLD}{url}{C_RESET} ({name}) ({}) ...{C_RESET}",
-                                     STRING_LOADER.load("SyncingPackageDatabase"),
-                                     format_size(content_size)));
-
-        /* delete_last_line();
-        println!("{GREEN}{BOLD} ✓ {C_RESET} {} {CYAN}{BOLD}{url}{C_RESET} ({name}) ({}) ...{C_RESET}",
-                 STRING_LOADER.load("SyncingPackageDatabase"),
-                 format_size(content_size)
-        ); */
+        sp.stop_with_message(format!(
+            "{GREEN}{BOLD} ✓ {C_RESET} {} {CYAN}{BOLD}{url}{C_RESET} ({name}) ({}) ...{C_RESET}", 
+            STRING_LOADER.load("SyncingPackageDatabase"), format_size(content_size)
+            ));
 
         let mut counter = success_counter.lock().unwrap();
         *counter += 1;
 
     }
-
-    // println!("{}", success_counter.lock().unwrap());
-    // println!("{}", unsuccess_counter.lock().unwrap());
 
     if *failed_counter.lock().unwrap() >= 1 && *success_counter.lock().unwrap() == 0 {
         eprintln!("{}{C_RESET}", STRING_LOADER.load("UnsuccessfulSyncingPackageDatabase"));
@@ -91,9 +95,7 @@ pub async fn sync() {
     }
     else if *failed_counter.lock().unwrap() >= 1 && *success_counter.lock().unwrap() >= 1 {
         eprintln!("{YELLOW}{BOLD} ! {C_RESET} {}{C_RESET}", STRING_LOADER.load("AtLeastOneUnsuccessfulSyncingPackageDatabase"));
-        println!("{}", STRING_LOADER.load_with_params("SuccessSyncingPackageDatabase", &[&format!("{:.2}", start_time.elapsed().as_secs_f64())]));
     }
-    else {
-        println!("{}", STRING_LOADER.load_with_params("SuccessSyncingPackageDatabase", &[&format!("{:.2}", start_time.elapsed().as_secs_f64())]));
-    }
+    
+    println!("{}", STRING_LOADER.load_with_params("SuccessSyncingPackageDatabase", &[&format!("{:.2}", start_time.elapsed().as_secs_f64())]));
 }
