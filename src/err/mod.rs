3c54db44e88f8c578bb612a2d79e3b8af09d52e8
Juliandev02 <julian@strawberryfoundations.xyz>
2024-05-10 19:05:27 +0200

    changes to error handler

--- src/err/mod.rs
+++ src/err/mod.rs
@@ -5,19 +5,23 @@ use stblib::colors::{BOLD, C_RESET, CYAN, RED};
 
 #[derive(Debug, Eq, PartialEq)]
 pub enum SpkgError {
-    HttpError,
-    MissingPermissions(&'static str)
+    HttpError(&'static String, &'static String),
+    MissingPermissions(&'static str),
+    ConfigError(String),
 }
 
-pub fn throw(error: SpkgError, params: &[&dyn std::fmt::Display]) {
+pub fn throw(error: SpkgError) {
     match error {
-        SpkgError::HttpError => {
-            eprintln!("{RED}{BOLD} × {C_RESET} {} {CYAN}{BOLD}{}{C_RESET} ({}){C_RESET}", STRING_LOADER.load("SyncingPackageDatabase"), params[0], params[1]);
+        SpkgError::HttpError(url, name) => {
+            eprintln!("{RED}{BOLD} × {C_RESET} {} {CYAN}{BOLD}{}{C_RESET} ({}){C_RESET}", STRING_LOADER.load("SyncingPackageDatabase"), url, name);
             eprintln!("{RED}{BOLD} ↳  {}{C_RESET}", STRING_LOADER.load("HttpError"));
         },
         SpkgError::MissingPermissions(suberror) => {
             eprintln!("{CYAN}{BOLD}{}:{C_RESET} {}", SPKG_DIRECTORIES.mirrors, STRING_LOADER.load("MissingPermissions"));
             eprintln!("{}", STRING_LOADER.load(suberror));
+        },
+        SpkgError::ConfigError(error) => {
+            eprintln!("{} ({}){C_RESET}", STRING_LOADER.load("InvalidConfig"), error);
         }
     }
 }
