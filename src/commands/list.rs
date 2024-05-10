b49fdcc1461392d60a2d5476c56b2d37b0504db3
Juliandev02 <julian@strawberryfoundations.xyz>
2024-05-10 19:36:45 +0200

    list command will now also show packages from another repositories

--- src/commands/list.rs
+++ src/commands/list.rs
@@ -1,34 +1,65 @@
-use sqlx::Row;
+use sqlx::FromRow;
 use stblib::colors::{BOLD, C_RESET, CYAN, GREEN, RESET};
 use crate::cli::SPKG_OPTIONS;
 use crate::db::db::Database;
-use crate::spkg_core::SPKG_FILES;
+use crate::spkg_core::{CONFIG, SPKG_FILES};
+
+#[derive(FromRow, Debug)]
+pub struct SpkgListPackage {
+    pub name: String,
+    pub version: String,
+    pub branch: String,
+    pub arch: String,
+}
+
 
 pub async fn list() {
-    let db = if SPKG_OPTIONS.list_installed {
-        Database::new(&SPKG_FILES.world_database).await
-    }
-    else {
-        Database::new(&SPKG_FILES.package_database).await
+    if SPKG_OPTIONS.list_installed {
+        let db = Database::new(&SPKG_FILES.world_database).await;
+
+        let mut packages: Vec<SpkgListPackage> = if SPKG_OPTIONS.list_custom_arch {
+            sqlx::query_as("SELECT * FROM packages WHERE arch = ? ORDER BY name GLOB '[A-Za-z]*' DESC, name")
+                .bind(&SPKG_OPTIONS.list_custom_arch_type)
+                .fetch_all(&db.connection)
+                .await.unwrap()
+        } else {
+            sqlx::query_as("SELECT * FROM packages ORDER BY name GLOB '[A-Za-z]*' DESC, name")
+                .fetch_all(&db.connection)
+                .await.unwrap()
+        };
+
+        packages.sort_by(|a, b| a.name.cmp(&b.name));
+
+        for entry in packages {
+            println!(
+                "{GREEN}{BOLD}{}{C_RESET} ({}) @ {CYAN} {}{RESET}/{}",
+                entry.name, entry.version, entry.branch, entry.arch
+            )
+        }
+        
+        std::process::exit(0);
     };
 
-    let entries = if SPKG_OPTIONS.list_custom_arch {
-        sqlx::query("SELECT * FROM packages WHERE arch = ? ORDER BY name GLOB '[A-Za-z]*' DESC, name")
+    let mut packages: Vec<SpkgListPackage> = vec![];
+
+    for (name, _) in CONFIG.repositories.iter() {
+        let db_location = &SPKG_FILES.package_database.replace("main.db", format!("{name}.db").as_str());
+        let db = Database::new(db_location).await;
+
+        let pkg: Vec<SpkgListPackage> = sqlx::query_as("SELECT * FROM packages ORDER BY name GLOB '[A-Za-z]*' DESC, name")
             .bind(&SPKG_OPTIONS.list_custom_arch_type)
             .fetch_all(&db.connection)
-            .await.unwrap()
-    } else {
-        sqlx::query("SELECT * FROM packages ORDER BY name GLOB '[A-Za-z]*' DESC, name")
-            .fetch_all(&db.connection)
-            .await.unwrap()
+            .await.unwrap();
+
+        packages.extend(pkg);
     };
 
-    for entry in entries {
-        let name: String = entry.get("name");
-        let version: String = entry.get("version");
-        let branch: String = entry.get("branch");
-        let architecture: String = entry.get("arch");
+    packages.sort_by(|a, b| a.name.cmp(&b.name));
 
-        println!("{GREEN}{BOLD}{name}{C_RESET} ({version}) @ {CYAN} {branch}{RESET}/{architecture}");
+    for entry in packages {
+        println!(
+            "{GREEN}{BOLD}{}{C_RESET} ({}) @ {CYAN} {}{RESET}/{}",
+            entry.name, entry.version, entry.branch, entry.arch
+        );
     }
 }
