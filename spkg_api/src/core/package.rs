db35bd5a914eae480f4d5e22eba10b7167ac75f8
Juliandev02 <julian@strawberryfoundations.xyz>
2024-05-26 18:54:12 +0200

    added spkg_api

---
+++ spkg_api/src/core/package.rs
@@ -0,0 +1,12 @@
+use sqlx::FromRow;
+
+#[derive(FromRow)]
+pub struct Package {
+    pub name: String,
+    pub version: String,
+    pub branch: String,
+    pub arch: String,
+    pub url: String,
+    pub specfile: String,
+    pub filename: String,
+}
