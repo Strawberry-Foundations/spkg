6ba5a77a10e2e40a0527f931c976d1548b361635
Juliandev02 <julian@strawberryfoundations.xyz>
2024-05-26 18:53:59 +0200

    added spkg_api

--- src/commands/download.rs
+++ src/commands/download.rs
@@ -1,13 +1,28 @@
 use std::env;
 
 use crate::cli::SPKG_OPTIONS;
+use crate::commands::list::SpkgListPackage;
 use crate::db::db::Database;
-use crate::spkg_core::package::package_download;
-use crate::spkg_core::SPKG_FILES;
+use crate::spkg_core::package::{Package, package_download};
+use crate::spkg_core::{CONFIG, SPKG_FILES};
 
 pub async fn download() {
     let packages: Vec<String> = env::args().skip(2).collect();
 
+    let mut db_packages: Vec<Package> = vec![];
+
+    for (name, _) in CONFIG.repositories.iter() {
+        let db_location = &SPKG_FILES.package_database.replace("main.db", format!("{name}.db").as_str());
+        let db = Database::new(db_location).await;
+
+        let pkg: Vec<Package> = 
+            sqlx::query_as("SELECT * FROM packages")
+                .fetch_all(&db.connection)
+                .await.unwrap();
+
+        db_packages.extend(pkg);
+    };
+    
     let db = Database::new(&SPKG_FILES.package_database).await;
 
     if packages.len() <= 1 {
