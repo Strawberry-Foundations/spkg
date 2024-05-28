7cbef427623df728c1ef863faeb34f20ad1c76ba
Juliandev02 <julian@strawberryfoundations.xyz>
2024-05-28 22:20:49 +0200

    impl: Updated core

--- src/core/package.rs
+++ src/core/package.rs
@@ -1,4 +1,7 @@
 use sqlx::FromRow;
+use std::collections::HashMap;
+use crate::core::db::Database;
+use crate::core::SPKG_FILES;
 
 #[derive(FromRow, Debug)]
 pub struct Package {
@@ -19,3 +22,34 @@ pub struct BasePackage {
     pub arch: String,
 }
 
+
+
+pub struct PackageList {
+    pub packages: Vec<Package>
+}
+
+impl PackageList {
+    pub async fn new(repo_list: &HashMap<String, String>) -> Self {
+        let mut packages: Vec<Package> = vec![];
+
+        for (name, _) in repo_list.iter() {
+            let db_location = &SPKG_FILES.package_database.replace("main.db", format!("{name}.db").as_str());
+            let db = Database::new(db_location).await.unwrap();
+
+            let pkg: Vec<Package> =
+                sqlx::query_as("SELECT * FROM packages")
+                    .fetch_all(&db.connection)
+                    .await.unwrap();
+
+            packages.extend(pkg);
+        }
+
+        Self {
+            packages
+        }
+    }
+
+    pub fn get(&self, package_name: String) -> &Package {
+        self.packages.iter().find(|package| package.name == package_name).unwrap()
+    }
+}
