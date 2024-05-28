1eead225d4d985c4c24f82baceefa7e69d4c773b
Juliandev02 <julian@strawberryfoundations.xyz>
2024-05-28 21:29:35 +0200

    impl: Added commands module

--- src/main.rs
+++ src/main.rs
@@ -2,10 +2,14 @@ use crate::cli::args::{Args, Command};
 
 pub mod cli;
 pub mod err;
+mod commands;
 
 #[tokio::main]
 async fn main() {
     match Args::new().command {
+        Command::Help => {
+
+        }
         Command::Install(package, options) => {
             println!("{package} - {options:?}")
         }
