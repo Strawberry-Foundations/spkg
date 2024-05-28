68e92a3adc50fa819dbfa206ed9535816db958b9
Juliandev02 <julian@strawberryfoundations.xyz>
2024-05-28 22:45:05 +0200

    impl: Finalized list command

--- src/commands/list.rs
+++ src/commands/list.rs
@@ -1,11 +1,19 @@
 use stblib::colors::{BOLD, C_RESET, CYAN, GREEN, RESET};
+
 use crate::cli::args::CommandOptions;
 use crate::core::CONFIG;
-use crate::core::package::PackageList;
+use crate::core::package::{BasePackageList, PackageList};
 
 pub async fn list(options: CommandOptions) {
     if options.installed {
+        let packages = BasePackageList::new_local().await;
 
+        for entry in packages {
+            println!(
+                "{GREEN}{BOLD}{}{C_RESET} ({}) @ {CYAN} {}{RESET}/{}",
+                entry.name, entry.version, entry.branch, entry.arch
+            );
+        }
     }
 
     let packages = PackageList::new(&CONFIG.repositories).await;
@@ -16,5 +24,4 @@ pub async fn list(options: CommandOptions) {
             entry.name, entry.version, entry.branch, entry.arch
         );
     }
-
 }
