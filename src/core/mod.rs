3b87c426e0a2609ec2827289a7e0ead210a10eaf
Juliandev02 <julian@strawberryfoundations.xyz>
2024-05-28 21:44:07 +0200

    impl: Updated config

--- src/core/mod.rs
+++ src/core/mod.rs
@@ -1,9 +1,14 @@
 mod config;
+mod fs;
 
 use lazy_static::lazy_static;
 use stblib::strings::Strings;
+use crate::utilities::get_language_strings;
 
 lazy_static! {
+    pub static ref SPKG_DIRECTORIES: fs::SpkgDirectories = fs::SpkgDirectories::new();
+    pub static ref SPKG_FILES: fs::SpkgFiles = fs::SpkgFiles::new();
+
     pub static ref CONFIG: config::Config = config::Config::new();
     pub static ref STRINGS: Strings = Strings::new_with_placeholders(&CONFIG.language, get_language_strings().as_str());
 }
