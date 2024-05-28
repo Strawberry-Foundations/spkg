9f9a7e7348344fe57cd82974bad31e9882776610
Juliandev02 <julian@strawberryfoundations.xyz>
2024-05-28 21:44:02 +0200

    impl: Updated utilities.rs

--- src/utilities.rs
+++ src/utilities.rs
@@ -1,5 +1,6 @@
 use std::{fs, io};
 use std::io::Write;
+use crate::core::{CONFIG, SPKG_DIRECTORIES};
 
 pub fn open_file(path: &str) -> String {
     fs::read_to_string(path).expect("Could not read file")
@@ -9,4 +10,8 @@ pub fn delete_last_line() {
     print!("\x1b[1A");
     print!("\x1b[2K");
     io::stdout().flush().unwrap();
+}
+
+pub fn get_language_strings() -> String {
+    open_file(format!("{}{}.yml", &SPKG_DIRECTORIES.language_files, CONFIG.language).as_str())
 }
