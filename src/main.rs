cc2a0d1d0a4e68db9f7309256630d20a12a16563
Juliandev02 <julian@strawberryfoundations.xyz>
2024-05-28 22:01:18 +0200

    impl: Updated main

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 #![allow(dead_code)]
 
 use crate::cli::args::{Args, Command};
+use crate::core::STRINGS;
+use crate::statics::VERSION;
 
 pub mod cli;
 pub mod err;
@@ -13,15 +15,19 @@ mod utilities;
 async fn main() {
     match Args::new().command {
         Command::Help => {
-
+            println!("{}", STRINGS.load_with_params("Help", &[&VERSION, &std::env::consts::ARCH]))
         }
         Command::Install(package, options) => {
             println!("{package} - {options:?}")
         }
+        Command::Info(package, options) => {
+            println!("{package} - {options:?}")
+        }
+        Command::List(..) => {
+            
+        }
         Command::Err(error) => {
             eprintln!("{error}")
-
         }
-        _ => {}
     }
 }
