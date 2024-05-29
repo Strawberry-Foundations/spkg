bf171f68b0c2e058c19adbf04edc36ef956a1589
Juliandev02 <julian@strawberryfoundations.xyz>
2024-05-29 21:51:37 +0200

    impl: changes to download

--- src/commands/download.rs
+++ src/commands/download.rs
@@ -1,14 +1,14 @@
-use stblib::colors::{BOLD, C_RESET, UNDERLINE};
-
 use crate::cli::args::CommandOptions;
-use crate::core::{CONFIG, STRINGS};
+use crate::core::CONFIG;
 use crate::core::package::{get_package, PackageList};
 
-pub async fn download(package: String, options: CommandOptions) -> eyre::Result<()> {
+pub async fn download(package: Vec<String>, options: CommandOptions) -> eyre::Result<()> {
     let packages = PackageList::new(&CONFIG.repositories).await;
-    let package = get_package(package, packages, options)?;
     
-    package.download().await?;
-
+    if package.len() == 1 {
+        let package = get_package(&package[0], packages, options)?;
+        package.download().await?;
+    }
+    
     Ok(())
 }
