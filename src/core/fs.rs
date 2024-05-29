2644ba028c9d4ad547f370c44bcdfefca0259ae5
Juliandev02 <julian@strawberryfoundations.xyz>
2024-05-29 20:59:50 +0200

    impl: Updated core

--- src/core/fs.rs
+++ src/core/fs.rs
@@ -57,3 +57,16 @@ impl SpkgFiles {
         }
     }
 }
+
+pub fn format_size(bytes: u64) -> String {
+    const KB: u64 = 1024;
+    const MB: u64 = 1024 * KB;
+
+    if bytes < KB {
+        format!("{} B", bytes)
+    } else if bytes < MB {
+        format!("{:.1} kB", bytes as f64 / KB as f64)
+    } else {
+        format!("{:.1} MB", bytes as f64 / MB as f64)
+    }
+}
