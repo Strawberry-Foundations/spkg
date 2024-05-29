9b0d4ad8f6020746f37d2587b547b770cb8a44c4
Juliandev02 <julian@strawberryfoundations.xyz>
2024-05-29 21:55:53 +0200

    impl: changes to download

--- src/commands/download.rs
+++ src/commands/download.rs
@@ -3,11 +3,18 @@ use crate::core::CONFIG;
 use crate::core::package::{get_package, PackageList};
 
 pub async fn download(package: Vec<String>, options: CommandOptions) -> eyre::Result<()> {
-    let packages = PackageList::new(&CONFIG.repositories).await;
+    let mut packages = PackageList::new(&CONFIG.repositories).await;
     
     if package.len() == 1 {
-        let package = get_package(&package[0], packages, options)?;
+        let package = get_package(&package[0], &mut packages, &options)?;
         package.download().await?;
+    }
+    else {
+        for p in package {
+            let package = get_package(&p, &mut packages, &options)?;
+            package.download().await?;    
+        }
+        
     }
     
     Ok(())
