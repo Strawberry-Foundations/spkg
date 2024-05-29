de2684bf4e87d08d5e93523b863a08d75c52f9d7
Juliandev02 <julian@strawberryfoundations.xyz>
2024-05-29 21:38:41 +0200

    impl: added get_package()

--- src/commands/download.rs
+++ src/commands/download.rs
@@ -1,30 +1,12 @@
-use eyre::Report;
 use stblib::colors::{BOLD, C_RESET, UNDERLINE};
 
 use crate::cli::args::CommandOptions;
 use crate::core::{CONFIG, STRINGS};
-use crate::core::package::PackageList;
-use crate::err::spkg::SpkgError;
+use crate::core::package::{get_package, PackageList};
 
 pub async fn download(package: String, options: CommandOptions) -> eyre::Result<()> {
-    let mut packages = PackageList::new(&CONFIG.repositories).await;
-
-    let package = if let Some(arch) = options.arch {
-        match packages.find(|p| p.name == package && p.arch == arch) {
-            Some(package) => package,
-            None => {
-                return Err(Report::from(SpkgError::PackageNotFound))
-            }
-        }
-    }
-    else {
-        match packages.find(|p| p.name == package && (p.arch == "all" || p.arch == std::env::consts::ARCH)) {
-            Some(package) => package,
-            None => {
-                return Err(Report::from(SpkgError::PackageNotFound))
-            }
-        }
-    };
+    let packages = PackageList::new(&CONFIG.repositories).await;
+    let package = get_package(package, packages, options).unwrap();
 
     println!("{BOLD}{UNDERLINE}{} {} ({}){C_RESET}", STRINGS.load("PackageInformationTitle"), package.name, package.version);
     println!("{}: {}", STRINGS.load("Name"), package.name);
