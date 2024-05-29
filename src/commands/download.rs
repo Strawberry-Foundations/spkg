ea37dbd6eb4818eb483b019e6bedeabf40e8a12a
Juliandev02 <julian@strawberryfoundations.xyz>
2024-05-29 21:47:59 +0200

    impl: added download command

--- src/commands/download.rs
+++ src/commands/download.rs
@@ -6,15 +6,9 @@ use crate::core::package::{get_package, PackageList};
 
 pub async fn download(package: String, options: CommandOptions) -> eyre::Result<()> {
     let packages = PackageList::new(&CONFIG.repositories).await;
-    let package = get_package(package, packages, options).unwrap();
-
-    println!("{BOLD}{UNDERLINE}{} {} ({}){C_RESET}", STRINGS.load("PackageInformationTitle"), package.name, package.version);
-    println!("{}: {}", STRINGS.load("Name"), package.name);
-    println!("{}: {}", STRINGS.load("Version"), package.version);
-    println!("{}: {}", STRINGS.load("Branch"), package.branch);
-    println!("{}: {}", STRINGS.load("Architecture"), package.arch);
-    println!("{}: {}", STRINGS.load("PackageUrl"), package.url);
-    println!("{}: {}", STRINGS.load("SpecfileUrl"), package.specfile);
+    let package = get_package(package, packages, options)?;
+    
+    package.download().await?;
 
     Ok(())
 }
