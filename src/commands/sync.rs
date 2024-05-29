f5dbb4917db0ffbd8b76bfe56519bf140b747e87
Juliandev02 <julian@strawberryfoundations.xyz>
2024-05-29 21:13:25 +0200

    impl: Updated sync command

--- src/commands/sync.rs
+++ src/commands/sync.rs
@@ -1,5 +1,6 @@
 use std::sync::{Arc, Mutex};
 use std::time::{Instant};
+use eyre::Report;
 
 use tokio::fs::File;
 use tokio::io::AsyncWriteExt;
@@ -9,14 +10,14 @@ use futures_util::stream::StreamExt;
 use stblib::colors::{BOLD, C_RESET, CYAN, GREEN, YELLOW};
 use crate::cli::args::CommandOptions;
 
-use crate::err;
 use crate::spinners::{Spinner, Spinners};
 use crate::core::{CONFIG, SPKG_DIRECTORIES, STRINGS};
 use crate::core::fs::format_size;
+use crate::err::fs::PermissionsError;
 use crate::net::http::remote_header;
 use crate::utilities::delete_last_line;
 
-pub async fn sync(options: CommandOptions) {
+pub async fn sync(options: CommandOptions) -> eyre::Result<()> {
     let start_time = Instant::now();
 
     let success_counter = Arc::new(Mutex::new(0));
@@ -46,23 +47,29 @@ pub async fn sync(options: CommandOptions) {
         };
 
         if response.status().is_success() {
-            let mut database = File::create(database_local).await.unwrap_or_else(|_| {
-                sp.stop_with_message(String::new());
-                delete_last_line();
-                std::process::exit(1);
-            });
+            let mut database = match File::create(database_local).await {
+                Ok(file) => file,
+                Err(_) => {
+                    sp.stop_with_message(String::new());
+                    delete_last_line();
+                    return Err(Report::from(PermissionsError::MissingPermissionsPackageDatabaseUpdate))
+                }
+            };
 
             let mut stream = response.bytes_stream();
 
             while let Some(item) = stream.next().await {
                 let chunk = item;
 
-                database.write_all(&chunk.unwrap()).await.unwrap_or_else(|_| {
-                    sp.stop_with_message(String::new());
-                    delete_last_line();
-                    std::process::exit(1);
-                });
-            }
+                match database.write_all(&chunk.unwrap()).await {
+                    Ok(..) => (),
+                    Err(_) => {
+                        sp.stop_with_message(String::new());
+                        delete_last_line();
+                        return Err(Report::from(PermissionsError::MissingPermissionsPackageDatabaseUpdate))
+                    }
+                }
+            }   
         }
 
         else {
@@ -94,4 +101,5 @@ pub async fn sync(options: CommandOptions) {
     }
 
     println!("{}", STRINGS.load_with_params("SuccessSyncingPackageDatabase", &[&format!("{:.2}", start_time.elapsed().as_secs_f64())]));
+    Ok(())
 }
