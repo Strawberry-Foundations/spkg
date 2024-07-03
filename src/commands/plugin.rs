75853c3c98d6fe31c4c586d6089b03fa423e94f9
Juliandev02 <julian@strawberryfoundations.xyz>
2024-07-03 16:11:53 +0200

    feat: first implementations for plugin system

--- src/commands/plugin.rs
+++ src/commands/plugin.rs
@@ -1,16 +1,21 @@
 use libloading::{Library, Symbol};
 use libspkg::plugin::Plugin;
 use std::path::PathBuf;
+use stblib::colors::{BOLD, C_RESET, CYAN, GREEN};
 
 pub fn execute() {
-    let plugin_path = PathBuf::from("/home/julian/Projekte/libspkg/example_plugin/target/release/libexample_plugin.so");
+    let path = PathBuf::from("/home/julian/Projekte/libspkg/example_plugin/target/release/libexample_plugin.so");
     
     unsafe {
-        let lib = Library::new(plugin_path).expect("Could not load library");
+        let lib = Library::new(&path).expect("Could not load library");
         let create_plugin: Symbol<unsafe extern "C" fn() -> Box<dyn Plugin>> = lib.get(b"create_plugin").expect("Could not load symbol");
         let plugin = create_plugin();
 
-        println!("Loaded plugin: {}", plugin.properties().name);
+        println!("Loaded plugin {CYAN}{}{C_RESET}", path.file_name().unwrap().to_str().unwrap());
+        println!("Plugin Name: {GREEN}{BOLD}{}{C_RESET}", plugin.properties().name);
+        println!("Plugin Version: {GREEN}{BOLD}{}{C_RESET}", plugin.properties().version);
+        println!("Plugin ID: {GREEN}{BOLD}{}{C_RESET}", plugin.properties().id);
+        println!("Plugin Package ID: {GREEN}{BOLD}{}{C_RESET}\n", plugin.properties().package_id);
         plugin.execute();
     }
 }
