36dba237303d253ad6a7dc987400c1550700e6c7
Juliandev02 <julian@strawberryfoundations.xyz>
2024-07-03 17:25:55 +0200

    feat: Updated info command

--- src/commands/info.rs
+++ src/commands/info.rs
@@ -1,4 +1,4 @@
-use stblib::colors::{BOLD, C_RESET, UNDERLINE};
+use stblib::colors::{BOLD, C_RESET, CYAN, GREEN, UNDERLINE};
 
 use crate::cli::args::CommandOptions;
 use crate::core::{CONFIG, STRINGS};
@@ -8,13 +8,12 @@ pub async fn info(package: String, options: CommandOptions) -> eyre::Result<()> 
     let mut packages = PackageList::new(&CONFIG.repositories).await;
     let package = get_package(&package, &mut packages, &options)?;
 
-    println!("{BOLD}{UNDERLINE}{} {} ({}){C_RESET}", STRINGS.load("PackageInformationTitle"), package.name, package.version);
-    println!("{}: {}", STRINGS.load("Name"), package.name);
-    println!("{}: {}", STRINGS.load("Version"), package.version);
-    println!("{}: {}", STRINGS.load("Branch"), package.branch);
-    println!("{}: {}", STRINGS.load("Architecture"), package.arch);
-    println!("{}: {}", STRINGS.load("PackageUrl"), package.url);
-    println!("{}: {}", STRINGS.load("SpecfileUrl"), package.specfile);
+    println!("{BOLD}{UNDERLINE}{CYAN}{} {} ({}){C_RESET}", STRINGS.load("PackageInformationTitle"), package.name, package.version);
+    println!("{}: {GREEN}{BOLD}{}{C_RESET}", STRINGS.load("Name"), package.name);
+    println!("{}: {GREEN}{BOLD}{}{C_RESET}", STRINGS.load("Version"), package.version);
+    println!("{}: {GREEN}{BOLD}{}{C_RESET}", STRINGS.load("Branch"), package.branch);
+    println!("{}: {GREEN}{BOLD}{}{C_RESET}", STRINGS.load("Architecture"), package.arch);
+    println!("{}: {GREEN}{BOLD}{}{C_RESET}", STRINGS.load("SpecfileUrl"), package.specfile);
 
     Ok(())
 }
