f5aa52c8c73d2cf0df6efc22d25c851e702387f3
Juliandev02 <julian@strawberryfoundations.xyz>
2024-07-04 19:30:10 +0200

    feat: Added plugin list command

--- src/commands/plugin.rs
+++ src/commands/plugin.rs
@@ -1,22 +1,59 @@
+use std::fs;
+use std::path::{Path, PathBuf};
+
 use libloading::{Library, Symbol};
 use libspkg::plugin::Plugin;
-use std::path::PathBuf;
 use stblib::colors::{BOLD, C_RESET, CYAN, GREEN};
 
+use crate::core::{SPKG_DIRECTORIES, STRINGS};
+
 pub fn main(args: Vec<String>) {
-    println!("{args:?}")
+    match args.first().unwrap_or(&String::from("")).as_str() {
+        "list" => {
+            list()
+        }
+        "info" => {
+            list()
+        }
+        _ => {}
+    }
 }
 
 pub fn list() {
-    
+    let path = format!("{}plugins", &SPKG_DIRECTORIES.system_config);
+    let dir_path = Path::new(&path);
+
+    match fs::read_dir(dir_path) {
+        Ok(entries) => {
+            for entry in entries {
+                match entry {
+                    Ok(entry) => unsafe {
+                        let path = entry.path();
+                        if !path.is_dir() {
+                            let lib = Library::new(&path).expect("Could not load library");
+                            let create_plugin: Symbol<unsafe extern "C" fn() -> Box<dyn Plugin>> = lib.get(b"create_plugin").expect("Could not load symbol");
+                            let plugin = create_plugin();
+
+                            println!("{BOLD}* {CYAN}{} ({}){C_RESET}", path.file_name().unwrap().to_str().unwrap(), plugin.properties().id);
+                            println!("   - {}: {GREEN}{BOLD}{}{C_RESET}", STRINGS.load("Name"), plugin.properties().name);
+                            println!("   - {}: {GREEN}{BOLD}{}{C_RESET}", STRINGS.load("Version"), plugin.properties().version);
+                            println!("   - {}: {GREEN}{BOLD}{}{C_RESET}", STRINGS.load("PackageId"), plugin.properties().package_id);
+                        }
+                    }
+                    Err(e) => println!("{e}"),
+                }
+            }
+        }
+        Err(e) => println!("{e}"),
+    }
 }
 
 pub fn register() {
-    
+
 }
 
 pub fn execute() {
-    let path = PathBuf::from("/home/julian/Projekte/libspkg/example_plugin/target/release/libexample_plugin.so");
+    let path = PathBuf::from("./data/etc/spkg/plugins/libexample_plugin.so");
     
     unsafe {
         let lib = Library::new(&path).expect("Could not load library");
