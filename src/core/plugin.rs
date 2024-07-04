0b53c0714679bdb39d247e94aee395d51ca500e2
Juliandev02 <julian@strawberryfoundations.xyz>
2024-07-04 19:05:06 +0200

    feat: added core::plugin

---
+++ src/core/plugin.rs
@@ -0,0 +1,13 @@
+use std::path::PathBuf;
+use libloading::{Library, Symbol};
+use libspkg::plugin::Plugin;
+
+pub fn load_plugin(plugin_path: PathBuf) -> Box<dyn Plugin> {
+    unsafe {
+        let lib = Library::new(&plugin_path).expect("Could not load library");
+        let create_plugin: Symbol<unsafe extern "C" fn() -> Box<dyn Plugin>> = lib.get(b"create_plugin").expect("Could not load symbol");
+        let plugin = create_plugin();    
+        
+        return plugin
+    }
+}
