43974b965f8614324793b01b48044f79c2d17c73
Juliandev02 <julian@strawberryfoundations.xyz>
2024-07-08 16:10:58 +0200

    feat: updated install command

--- src/commands/install_src.rs
+++ src/commands/install_src.rs
@@ -2,7 +2,7 @@ use crate::cli::args::CommandOptions;
 use crate::core::CONFIG;
 use crate::core::package::PackageList;
 
-pub async fn install_src(packages: Vec<String>, options: CommandOptions) -> eyre::Result<()> {
+pub async fn install_src(packages: Vec<String>, options: &CommandOptions) -> eyre::Result<()> {
     let mut package_list = PackageList::new(&CONFIG.repositories).await;
 
     Ok(())
