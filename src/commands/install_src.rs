04fa587917644c418c9f02894da91982064c5e50
Juliandev02 <julian@strawberryfoundations.xyz>
2024-07-08 15:29:12 +0200

    feat: added base install commands

---
+++ src/commands/install_src.rs
@@ -0,0 +1,9 @@
+use crate::cli::args::CommandOptions;
+use crate::core::CONFIG;
+use crate::core::package::PackageList;
+
+pub async fn install_src(packages: Vec<String>, options: CommandOptions) -> eyre::Result<()> {
+    let mut package_list = PackageList::new(&CONFIG.repositories).await;
+
+    Ok(())
+}
