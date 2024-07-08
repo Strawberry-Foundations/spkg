c53067374248e667cdef408b35f118aea5acacc7
Juliandev02 <julian@strawberryfoundations.xyz>
2024-07-08 16:14:35 +0200

    fix: do_install() should not be public

--- src/commands/install_bin.rs
+++ src/commands/install_bin.rs
@@ -2,6 +2,10 @@ use crate::cli::args::CommandOptions;
 use crate::core::CONFIG;
 use crate::core::package::PackageList;
 
+async fn do_install(packages: Vec<String>, options: &CommandOptions, mut package_list: PackageList) -> eyre::Result<()> {
+    Ok(())    
+}
+
 pub async fn install_bin(packages: Vec<String>, options: &CommandOptions) -> eyre::Result<()> {
     let mut package_list = PackageList::new(&CONFIG.repositories).await;
 
