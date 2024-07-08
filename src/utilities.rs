f32cb5a0bbbe3d1517a6597e03fdeaae5a1feca6
Juliandev02 <julian@strawberryfoundations.xyz>
2024-07-08 16:54:31 +0200

    feat: Added get_basename() function

--- src/utilities.rs
+++ src/utilities.rs
@@ -1,5 +1,6 @@
 use std::{fs, io};
 use std::io::Write;
+use std::path::Path;
 use crate::core::{CONFIG, SPKG_DIRECTORIES};
 
 pub fn open_file(path: &str) -> String {
@@ -14,4 +15,9 @@ pub fn delete_last_line() {
 
 pub fn get_language_strings() -> String {
     open_file(format!("{}{}.yml", &SPKG_DIRECTORIES.language_files, CONFIG.language).as_str())
+}
+
+pub fn get_basename(path: &str) -> Option<String> {
+    let path = Path::new(path);
+    path.file_name()?.to_str().map(|s| s.to_string())
 }
