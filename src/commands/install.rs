27dd33d6ba8a84d85769949424f0460ac232da2d
Juliandev02 <julian@strawberryfoundations.xyz>
2024-07-08 16:10:56 +0200

    feat: updated install command

--- src/commands/install.rs
+++ src/commands/install.rs
@@ -1,7 +1,7 @@
 use std::env::consts::ARCH;
 use dialoguer::Select;
-use dialoguer::theme::{ColorfulTheme, Theme};
-use stblib::colors::{BOLD, RED, GREEN, C_RESET};
+use dialoguer::theme::{ColorfulTheme};
+use stblib::colors::{BOLD, RED};
 
 use crate::core::{CONFIG, STRINGS};
 use crate::core::package::{get_package, PackageList};
@@ -10,56 +10,58 @@ use crate::cli::args::CommandOptions;
 use crate::commands::install_bin::install_bin;
 use crate::commands::install_src::install_src;
 
+pub async fn do_install(packages: Vec<String>, options: &CommandOptions, mut package_list: PackageList) -> eyre::Result<()> {
+    let package = get_package(packages.first().unwrap(), &mut package_list, options)?;
+    let data = fetch_specfile(&package.specfile).await;
+
+    let binpkg_available = match ARCH {
+        "x86_64" => data.binpkg.as_ref().map_or(false, |binpkg| binpkg.x86_64.is_some()),
+        "aarch64" => data.binpkg.as_ref().map_or(false, |binpkg| binpkg.aarch64.is_some()),
+        _ => false,
+    };
+
+    if binpkg_available && data.srcpkg.is_some() {
+        println!("{}", STRINGS.load("BinPkgAndSrcPkgAvailable"));
+
+        let theme = ColorfulTheme::default();
+        let selector = Select::with_theme(&theme).clear(false).default(0);
+        let selection = selector
+            .items(&[STRINGS.load("BinPkg"), STRINGS.load("SrcPkg")])
+            .interact()
+            .unwrap();
+
+        match selection {
+            0 => {
+                println!("\n{}", STRINGS.load_with_params("PackageWillInstall", &[&STRINGS.load("BinPkg")]));
+                install_bin(packages, options).await?;
+            },
+            1 => {
+                println!("\n{}", STRINGS.load_with_params("PackageWillInstall", &[&STRINGS.load("SrcPkg")]));
+                install_src(packages, options).await?;
+            },
+            _ => unreachable!(),
+        }
+    }
+    else if binpkg_available && data.srcpkg.is_none() {
+        install_bin(packages, options).await?;
+    }
+    else if !binpkg_available && data.srcpkg.is_some() {
+        install_src(packages, options).await?;
+    }
+    else {
+        println!("{RED}{BOLD}{}", STRINGS.load("PackageNotAvailable"))
+    }
+
+    Ok(())
+}
 
 pub async fn install(packages: Vec<String>, options: CommandOptions) -> eyre::Result<()> {
-    let mut package_list = PackageList::new(&CONFIG.repositories).await;
-
     if packages.len() < 2 {
-        let package = get_package(packages.first().unwrap(), &mut package_list, &options)?;
-        let data = fetch_specfile(&package.specfile).await;
-
-        let binpkg_available = match ARCH {
-            "x86_64" => data.binpkg.as_ref().map_or(false, |binpkg| binpkg.x86_64.is_some()),
-            "aarch64" => data.binpkg.as_ref().map_or(false, |binpkg| binpkg.aarch64.is_some()),
-            _ => false,
-        };
-
-        if binpkg_available && data.srcpkg.is_some() {
-            println!("{}", STRINGS.load("BinPkgAndSrcPkgAvailable"));
-
-            let theme = ColorfulTheme::default();
-            let selector = Select::with_theme(&theme).clear(false).default(0);
-            let selection = selector
-                .items(&[STRINGS.load("BinPkg"), STRINGS.load("SrcPkg")])
-                .interact()
-                .unwrap();
-
-            match selection {
-                0 => {
-                    println!("\n{}", STRINGS.load_with_params("PackageWillInstall", &[&STRINGS.load("BinPkg")]));
-                    install_bin(packages, options).await?;
-                },
-                1 => {
-                    println!("\n{}", STRINGS.load_with_params("PackageWillInstall", &[&STRINGS.load("SrcPkg")]));
-                    install_src(packages, options).await?;
-                },
-                _ => unreachable!(),
-            }
-        }
-        else if binpkg_available && data.srcpkg.is_none() {
-            install_bin(packages, options).await?;
-        }
-        else if !binpkg_available && data.srcpkg.is_some() {
-            install_src(packages, options).await?;
-        }
-        else {
-            println!("{RED}{BOLD}{}", STRINGS.load("PackageNotAvailable"))
-        }
+        do_install(packages, &options, PackageList::new(&CONFIG.repositories).await).await?;
     }
     else {
         for package in packages {
-            let package = get_package(&package, &mut package_list, &options)?;
-            let data = fetch_specfile(&package.specfile).await;
+            do_install(vec![package], &options, PackageList::new(&CONFIG.repositories).await).await?;
         }
     }
     Ok(())
