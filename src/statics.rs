dd1650f7150735a45cb55fa8d18acd0a8166e720
Juliandev02 <julian@strawberryfoundations.xyz>
2024-07-08 22:06:14 +0200

    feat: developer mode is now only activated when using debug assertions

--- src/statics.rs
+++ src/statics.rs
@@ -1,2 +1,7 @@
 pub const VERSION: &str = "3.0.0b2";
+
+#[cfg(debug_assertions)]
 pub const DEVELOPMENT_MODE: bool = true;
+
+#[cfg(not(debug_assertions))]
+pub const DEVELOPMENT_MODE: bool = false;
