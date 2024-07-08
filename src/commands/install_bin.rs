87521c1a5ff1de00d162c480cb718e7d2e72e77a
Juliandev02 <julian@strawberryfoundations.xyz>
2024-07-08 16:21:07 +0200

    feat: Added checks before installing packages

--- src/commands/install_bin.rs
+++ src/commands/install_bin.rs
@@ -1,8 +1,25 @@
+use std::env::consts::ARCH;
 use crate::cli::args::CommandOptions;
-use crate::core::CONFIG;
-use crate::core::package::PackageList;
+use crate::core::{CONFIG, STRINGS};
+use crate::core::package::{get_package, PackageList};
+use crate::core::specfile::fetch_specfile;
 
 async fn do_install(packages: Vec<String>, options: &CommandOptions, mut package_list: PackageList) -> eyre::Result<()> {
+    let package = get_package(packages.first().unwrap(), &mut package_list, options)?;
+    let data = fetch_specfile(&package.specfile).await;
+
+    let binpkg_available = match ARCH {
+        "x86_64" => data.binpkg.as_ref().map_or(false, |binpkg| binpkg.x86_64.is_some()),
+        "aarch64" => data.binpkg.as_ref().map_or(false, |binpkg| binpkg.aarch64.is_some()),
+        _ => false,
+    };
+    
+    if !binpkg_available {
+        eprintln!("{}", STRINGS.load("PackageNotAvailableAsBinPkg"));
+        std::process::exit(1);
+    }
+    
+    
     Ok(())
 }
 
