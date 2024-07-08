1f7b95fb46b1dbbb4a5186549f0089348618b3c6
Juliandev02 <julian@strawberryfoundations.xyz>
2024-07-08 16:41:30 +0200

    fix: Fixed clippy warnings

--- src/commands/install_bin.rs
+++ src/commands/install_bin.rs
@@ -6,7 +6,7 @@ use crate::core::{CONFIG, STRINGS};
 use crate::core::package::{get_package, Package, PackageList};
 use crate::core::specfile::{fetch_specfile, Specfile};
 
-async fn do_install(package: Package, options: &CommandOptions, data: Specfile) -> eyre::Result<()> {
+async fn do_install(package: Package, _options: &CommandOptions, data: Specfile) -> eyre::Result<()> {
     let binpkg_available = match ARCH {
         "x86_64" => data.binpkg.as_ref().map_or(false, |binpkg| binpkg.x86_64.is_some()),
         "aarch64" => data.binpkg.as_ref().map_or(false, |binpkg| binpkg.aarch64.is_some()),
