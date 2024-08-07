176ac84fd80bad188adf77a1d5dde1a3f4e1d69f
Juliandev02 <julian@strawberryfoundations.xyz>
2024-08-07 21:34:07 +0200

    feat!: add complete new architecture management

--- src/commands/sync.rs
+++ src/commands/sync.rs
@@ -5,18 +5,18 @@ use eyre::Report;
 
 use tokio::fs::File;
 use tokio::io::AsyncWriteExt;
-
 use futures_util::stream::StreamExt;
 
-use stblib::colors::{BOLD, C_RESET, CYAN, GREEN, YELLOW};
-use crate::cli::args::CommandOptions;
+use stblib::colors::{BOLD, C_RESET, CYAN, GREEN, RED, YELLOW};
 
-use crate::spinners::{Spinner, Spinners};
 use crate::core::{CONFIG, SPKG_DIRECTORIES, STRINGS};
+use crate::core::config::AnyArch;
 use crate::core::fs::format_size;
 use crate::err::fs::PermissionsError;
 use crate::net::http::remote_header;
+use crate::cli::args::CommandOptions;
 use crate::utilities::delete_last_line;
+
 
 pub async fn sync(_: CommandOptions) -> eyre::Result<()> {
     let start_time = Instant::now();
@@ -27,6 +27,7 @@ pub async fn sync(_: CommandOptions) -> eyre::Result<()> {
     let mut existing_databases: Vec<String> = Vec::new();
     let mut configured_databases: Vec<String> = Vec::new();
 
+    // Get existing databases
     for entry in read_dir(&SPKG_DIRECTORIES.mirrors)? {
         let entry = entry?;
         let path = entry.path();
@@ -40,83 +41,97 @@ pub async fn sync(_: CommandOptions) -> eyre::Result<()> {
         }
     }
 
-    for (name, _) in CONFIG.repositories.iter() {
-        configured_databases.push(format!("{name}.db"));
+    // Get databases that needs to be updated
+    for (name, info) in CONFIG.repositories.iter() {
+        match info.arch {
+            AnyArch::Single(ref arch) => configured_databases.push(format!("{name}.{arch}.db")),
+            AnyArch::List(ref architectures) => {
+                for arch in architectures {
+                    configured_databases.push(format!("{name}.{arch}.db"))
+                }
+            }
+        }
     }
 
+    // Iterate every database that exists and check if the database is in configured_databases
     for database in existing_databases {
         if !configured_databases.contains(&database) {
             remove_file(format!("{}{database}", SPKG_DIRECTORIES.mirrors))?
         }
     }
 
-    for (name, url) in CONFIG.repositories.iter() {
-        let failed_counter_clone = failed_counter.clone();
+    for (name, info) in CONFIG.repositories.iter() {
+        for arch in info.arch.iter() {
+            let failed_counter_clone = failed_counter.clone();
 
-        let database_repo = format!("{url}/package.db");
-        let database_local = format!("{}{name}.db", SPKG_DIRECTORIES.mirrors);
-        let content_size = remote_header(&database_repo).await;
+            let database_repo = format!("{}/packages.{}.db", info.url, arch);
+            let database_local = format!("{}{name}.{arch}.db", SPKG_DIRECTORIES.mirrors);
+            let content_size = remote_header(&database_repo).await;
 
-        let mut sp = Spinner::new(
-            Spinners::Line, format!(
-                "{} {CYAN}{BOLD}{url}{C_RESET} ({name}) ...{C_RESET}",
-                STRINGS.load("SyncingPackageDatabase"))
-        );
+            let mut sp = crate::spinners::simple::SimpleSpinner::new();
+            sp.start(format!(
+                "{} {CYAN}{BOLD}{} {GREEN}{arch}{C_RESET} ({name}) ...{C_RESET}",
+                STRINGS.load("SyncingPackageDatabase"), info.url
+            ));
 
-        let Ok(response) = reqwest::get(database_repo.clone()).await else {
-            sp.stop_with_message(String::new());
-            delete_last_line();
+            let Ok(response) = reqwest::get(database_repo.clone()).await else {
+                sp.stop();
+                delete_last_line();
 
-            let mut counter = failed_counter_clone.lock().unwrap();
-            *counter += 1;
+                let mut counter = failed_counter_clone.lock().unwrap();
+                *counter += 1;
 
-            continue
-        };
-
-        if response.status().is_success() {
-            let mut database = match File::create(database_local).await {
-                Ok(file) => file,
-                Err(_) => {
-                    sp.stop_with_message(String::new());
-                    delete_last_line();
-                    return Err(Report::from(PermissionsError::MissingPermissionsPackageDatabaseUpdate))
-                }
+                continue
             };
 
-            let mut stream = response.bytes_stream();
-
-            while let Some(item) = stream.next().await {
-                let chunk = item;
-
-                match database.write_all(&chunk?).await {
-                    Ok(..) => (),
+            if response.status().is_success() {
+                let mut database = match File::create(database_local).await {
+                    Ok(file) => file,
                     Err(_) => {
-                        sp.stop_with_message(String::new());
+                        sp.stop();
                         delete_last_line();
                         return Err(Report::from(PermissionsError::MissingPermissionsPackageDatabaseUpdate))
                     }
+                };
+
+                let mut stream = response.bytes_stream();
+
+                while let Some(item) = stream.next().await {
+                    let chunk = item;
+
+                    match database.write_all(&chunk?).await {
+                        Ok(..) => (),
+                        Err(_) => {
+                            sp.stop();
+                            delete_last_line();
+                            return Err(Report::from(PermissionsError::MissingPermissionsPackageDatabaseUpdate))
+                        }
+                    }
                 }
             }
-        }
 
-        else {
-            sp.stop_with_message(String::new());
-            delete_last_line();
+            else {
+                 sp.stop_with_message(format!(
+                    "{RED}{BOLD} × {C_RESET} {} {CYAN}{BOLD}{} {GREEN}{arch}{C_RESET} ({name}){C_RESET}",
+                    STRINGS.load("SyncingPackageDatabase"), info.url
+                ));
+                sp.stop();
 
-            let mut counter = failed_counter_clone.lock().unwrap();
+                let mut counter = failed_counter_clone.lock().unwrap();
+                *counter += 1;
+
+                continue;
+            }
+
+            sp.stop_with_message(format!(
+                "{GREEN}{BOLD} ✓ {C_RESET} {} {CYAN}{BOLD}{} {GREEN}{arch}{C_RESET} ({name}) ({}) {C_RESET}",
+                STRINGS.load("SyncingPackageDatabase"), info.url, format_size(content_size)
+            ));
+
+            let mut counter = success_counter.lock().unwrap();
             *counter += 1;
 
-            continue;
         }
-
-        sp.stop_with_message(format!(
-            "{GREEN}{BOLD} ✓ {C_RESET} {} {CYAN}{BOLD}{url}{C_RESET} ({name}) ({}) ...{C_RESET}",
-            STRINGS.load("SyncingPackageDatabase"), format_size(content_size)
-        ));
-
-        let mut counter = success_counter.lock().unwrap();
-        *counter += 1;
-
     }
 
     if *failed_counter.lock().unwrap() >= 1 && *success_counter.lock().unwrap() == 0 {
