580159756a732fe5b43835867b0d787b08c823d8
Juliandev02 <julian@strawberryfoundations.xyz>
2024-08-14 21:29:42 +0200

    feat: add plugin err

---
+++ src/err/plugin.rs
@@ -0,0 +1,29 @@
+use std::error::Error;
+use std::fmt::{Display, Formatter, Result};
+use lazy_static::lazy_static;
+use stblib::colors::{BOLD, C_RESET, RED};
+
+use crate::core::STRINGS;
+
+
+#[derive(Debug)]
+pub enum PluginError {
+    PluginLoadError(String),
+}
+
+lazy_static! {
+    pub static ref FORMAT: String = format!("{RED}{BOLD}E: {C_RESET}");
+}
+
+impl Display for PluginError {
+    fn fmt(&self, f: &mut Formatter<'_>) -> Result {
+        match self {
+            PluginError::PluginLoadError(plugin) => {
+                write!(f, "{}{}", *FORMAT, STRINGS.load_with_params("PluginLoadError", &[plugin]))
+            },
+            
+        }
+    }
+}
+
+impl Error for PluginError {}
